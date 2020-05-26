657715999dc92e2969f4cbd56e91a18134b8d66e
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-05-26 15:59:15 +0300

    Initial commit

---
+++ app/src/main/java/com/alesat1215/productsfromerokhin/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.alesat1215.productsfromerokhin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
