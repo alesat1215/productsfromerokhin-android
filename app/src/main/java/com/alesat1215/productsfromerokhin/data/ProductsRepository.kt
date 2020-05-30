451545215b829dda0413a1665f6240750021b645
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-05-30 19:00:08 +0300

    add title methods

--- app/src/main/java/com/alesat1215/productsfromerokhin/data/ProductsRepository.kt
+++ app/src/main/java/com/alesat1215/productsfromerokhin/data/ProductsRepository.kt
@@ -2,9 +2,22 @@ package com.alesat1215.productsfromerokhin.data
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.alesat1215.productsfromerokhin.data.StartTitle.*
 
 class ProductsRepository {
     private val products = Array(10) { Product("Name name", price = 250) }.asList()
 
+    fun title(forType: StartTitle): LiveData<String> =
+        when (forType) {
+            TITLE -> MutableLiveData("Title")
+            IMAGE -> MutableLiveData("Image title")
+            LIST -> MutableLiveData("List title")
+            LIST2 -> MutableLiveData("List title2")
+        }
+
     fun products(): LiveData<List<Product>> = MutableLiveData(products)
+}
+
+enum class StartTitle {
+    TITLE, IMAGE, LIST, LIST2
 }
--- app/src/main/java/com/alesat1215/productsfromerokhin/start/StartViewModel.kt
+++ app/src/main/java/com/alesat1215/productsfromerokhin/start/StartViewModel.kt
@@ -5,10 +5,13 @@ import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 import com.alesat1215.productsfromerokhin.data.Product
 import com.alesat1215.productsfromerokhin.data.ProductsRepository
+import com.alesat1215.productsfromerokhin.data.StartTitle
 
 class StartViewModel: ViewModel() {
     private var repository = ProductsRepository()
     private var products = repository.products()
+
+    fun title(forType: StartTitle) = repository.title(forType)
 
     fun products(predicate: ((Product) -> Boolean)? = null): LiveData<List<Product>> {
         return if (predicate != null)
