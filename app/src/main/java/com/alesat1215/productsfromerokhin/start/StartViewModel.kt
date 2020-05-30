ad327e5881e7d0bb602b0beb581f5bfb728362a2
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-05-30 23:05:51 +0300

    bind start fragment_start.xml

--- app/src/main/java/com/alesat1215/productsfromerokhin/start/StartViewModel.kt
+++ app/src/main/java/com/alesat1215/productsfromerokhin/start/StartViewModel.kt
@@ -1,6 +1,8 @@
 package com.alesat1215.productsfromerokhin.start
 
+import androidx.annotation.Nullable
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 import com.alesat1215.productsfromerokhin.data.Product
