21fb1b42a5ccac2ab68ef5305f6259c0aece2d89
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-05-31 13:00:55 +0300

    del import

--- app/src/main/java/com/alesat1215/productsfromerokhin/start/StartFragment.kt
+++ app/src/main/java/com/alesat1215/productsfromerokhin/start/StartFragment.kt
@@ -3,7 +3,6 @@ package com.alesat1215.productsfromerokhin.start
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
