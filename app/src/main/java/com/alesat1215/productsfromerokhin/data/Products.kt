959415f2b77dcc912c8a717022bf4b866b3de6e4
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-05-31 21:03:41 +0300

    add room

--- app/src/main/java/com/alesat1215/productsfromerokhin/data/Products.kt
+++ app/src/main/java/com/alesat1215/productsfromerokhin/data/Products.kt
@@ -1,16 +1,37 @@
 package com.alesat1215.productsfromerokhin.data
 
-data class Groups(
-    var name: String? = null,
-    var index: Int? = null,
-    var img_path: String? = null,
+import androidx.room.*
+
+data class Products(
+    @Embedded val group: Group,
+    @Relation(
+        parentColumn = "index",
+        entityColumn = "groupIndex"
+    )
     var products: List<Product>? = null
 )
 
+@Fts4
+@Entity
+data class Group(
+    @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "rowid")
+    var groupId: Int,
+    var index: Int? = null,
+    var name: String? = null
+)
+
+@Fts4
+@Entity
 data class Product(
+    @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "rowid")
+    var productId: Int,
+    var groupIndex: Int,
+    var index: Int? = null,
     var name: String? = null,
     var last_name: String? = null,
-    var index: Int? = null,
+    var img_path: String? = null,
     var price: Int? = null,
-    var inCart: Int = 0
+    var inCart: Int = 0,
+    var inStartList: Boolean = false,
+    var inStartList2: Boolean = false
 )
