b5c3582cca4a6b9110e0c1bc7926645c54010ae7
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-05-31 12:49:42 +0300

    migrate to BindRVAdapter.kt

--- app/src/main/java/com/alesat1215/productsfromerokhin/start/StartFragment.kt
+++ app/src/main/java/com/alesat1215/productsfromerokhin/start/StartFragment.kt
@@ -5,14 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.ViewDataBinding
-import androidx.databinding.library.baseAdapters.BR
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.RecyclerView
+import com.alesat1215.productsfromerokhin.R
 import com.alesat1215.productsfromerokhin.data.Product
 import com.alesat1215.productsfromerokhin.databinding.FragmentStartBinding
-import com.alesat1215.productsfromerokhin.databinding.ListItemBinding
+import com.alesat1215.productsfromerokhin.util.BindRVAdapter
 
 /**
  * A simple [Fragment] subclass.
@@ -20,50 +19,23 @@ import com.alesat1215.productsfromerokhin.databinding.ListItemBinding
 class StartFragment : Fragment() {
 
     val viewModel: StartViewModel by viewModels()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return FragmentStartBinding.inflate(inflater, container, false).apply {
+    ) =
+        FragmentStartBinding.inflate(inflater, container, false).apply {
             viewModel = this@StartFragment.viewModel
             adapterToList(list) { (it.price ?: 0) == 250 }
             adapterToList(list2)
             executePendingBindings()
-//            viewModel.products { (it.price ?: 0) == 250 }
-//                .observe(viewLifecycleOwner, Observer {
-//                    list.adapter = MyAdapter(it)
-//                })
-//            viewModel.products()
-//                .observe(viewLifecycleOwner, Observer {
-//                    list2.adapter = MyAdapter(it)
-//                })
         }.root
-    }
 
     private fun adapterToList(list: RecyclerView, predicate: ((Product) -> Boolean)? = null) =
         viewModel.products(predicate).observe(viewLifecycleOwner, Observer {
-            list.swapAdapter(MyAdapter(it), false)
+            list.swapAdapter(BindRVAdapter(it, R.layout.list_item), false)
         })
 
 }
 
-class MyViewHolder(val binding: ViewDataBinding) : RecyclerView.ViewHolder(binding.root)
 
-class MyAdapter(private val dataSet: List<Product>): RecyclerView.Adapter<MyViewHolder>() {
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
-        MyViewHolder(ListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false))
-
-    override fun getItemCount() = dataSet.size
-
-    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-//        val product = dataset.get(position)
-//        holder.itemView.name.text = product.name
-//        holder.itemView.price.text = product.price.toString() + " P/kg"
-//        holder.itemView.productImage.setImageResource(androidx.vectordrawable.R.color.notification_action_color_filter)
-//        holder.itemView.count.text = "3"
-        holder.binding.setVariable(BR.product, dataSet[position])
-        holder.binding.executePendingBindings()
-    }
-}
