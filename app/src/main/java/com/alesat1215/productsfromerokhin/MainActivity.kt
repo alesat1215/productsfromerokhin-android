d1a356e8420cb4a465ad3654b045cbe911a78d55
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-06-02 15:26:06 +0300

    migrate to realtime database

--- app/src/main/java/com/alesat1215/productsfromerokhin/MainActivity.kt
+++ app/src/main/java/com/alesat1215/productsfromerokhin/MainActivity.kt
@@ -3,15 +3,18 @@ package com.alesat1215.productsfromerokhin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import com.alesat1215.productsfromerokhin.data.Group
+import com.alesat1215.productsfromerokhin.data.Products
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private val auth = FirebaseAuth.getInstance()
-    private val db = FirebaseFirestore.getInstance()
+    private val db = FirebaseDatabase.getInstance().reference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,19 +27,39 @@ class MainActivity : AppCompatActivity() {
         auth.signInAnonymously().addOnCompleteListener {
             if (it.isSuccessful) {
                 Log.d("firebase", auth.currentUser.toString())
-                db.collection("groups").addSnapshotListener { snapshot, exception ->
-                    if (exception != null) {
-                        Log.d("firebase", exception.localizedMessage)
-                        return@addSnapshotListener
+                db.addValueEventListener(object : ValueEventListener {
+                    override fun onCancelled(error: DatabaseError) {
+                        Log.d("firebase", error.message)
                     }
-                    if (snapshot != null) {
-                        Log.d("firebase", "${snapshot.size()}")
-                        val groups = snapshot.map { it.toObject(Group::class.java).apply { id = it.id } }
-                        for (group in groups) {
-                            Log.d("firebase", "${group.id} ${group.index} ${group.name}")
+
+                    override fun onDataChange(snapshot: DataSnapshot) {
+                        val products = snapshot.getValue(Products::class.java)
+//                        Log.d("firebase", "${products?.groups?.size}")
+//                        for (group in products?.groups ?: emptyList()) {
+//                            Log.d("firebase", "${group.products?.size} ${group.order}")
+//                        }
+                        for (product in products?.productsWithGroupOrder() ?: emptyList()) {
+                            Log.d("firebase", "${product.groupOrder} ${product.order}")
                         }
                     }
-                }
+
+                })
+//                db.collection("groups").addSnapshotListener { snapshot, exception ->
+//                    if (exception != null) {
+//                        Log.d("firebase", exception.localizedMessage)
+//                        return@addSnapshotListener
+//                    }
+//                    if (snapshot != null) {
+//                        Log.d("firebase", "${snapshot.size()}")
+//                        val groups = snapshot.map { it.toObject(Group::class.java).apply { id = it.id } }
+//                        for (group in groups) {
+//                            Log.d("firebase", "${group.id} ${group.index} ${group.name}")
+//                        }
+//                        val products = groups.map {
+//                            db.collection("groups").document(it.id).collection("products")
+//                        }
+//                    }
+//                }
             } else Log.d("firebase", "Login failed")
         }
     }
--- app/src/main/java/com/alesat1215/productsfromerokhin/data/Products.kt
+++ app/src/main/java/com/alesat1215/productsfromerokhin/data/Products.kt
@@ -3,35 +3,43 @@ package com.alesat1215.productsfromerokhin.data
 import androidx.room.*
 
 data class Products(
+    var groups: List<Group>? = null
+) {
+    fun productsWithGroupOrder(): List<Product>? = groups?.flatMap { group ->
+        group.products?.apply { map { it.groupOrder = group.order } } ?: emptyList()
+    }
+}
+
+data class ProductsDB(
     @Embedded val group: Group,
     @Relation(
-        parentColumn = "id",
-        entityColumn = "groupId"
+        parentColumn = "order",
+        entityColumn = "groupOrder"
     )
-    var products: List<Product>? = null
+    var products: List<Product>
 )
 
 @Fts4
 @Entity
 data class Group(
-//    @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "rowid")
-    var id: String = "",
-    var index: Int? = null,
-    var name: String? = null
+    @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "rowid")
+    var id: Int = 0,
+    var order: Int = 0,
+    var name: String? = null,
+    @Ignore var products: List<Product>? = null
 )
 
 @Fts4
 @Entity
 data class Product(
-//    @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "rowid")
-    var id: String = "",
-    var groupId: String = "",
-    var index: Int? = null,
+    @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "rowid")
+    var id: Int = 0,
+    var order: Int = 0,
+    var groupOrder: Int = 0,
     var name: String? = null,
-    var last_name: String? = null,
-    var img_path: String? = null,
+    var consist: String? = null,
+    var img: String? = null,
     var price: Int? = null,
-    var inCart: Int = 0,
-    var inStartList: Boolean = false,
-    var inStartList2: Boolean = false
+    var inStart: Boolean = false,
+    var inStart2: Boolean = false
 )
