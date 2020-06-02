7cec0ffce993bd65b4179e82dc3ee1411794b383
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-06-02 16:11:30 +0300

    migrate to data class RemoteData

--- app/src/main/java/com/alesat1215/productsfromerokhin/data/Products.kt
+++ app/src/main/java/com/alesat1215/productsfromerokhin/data/Products.kt
@@ -2,21 +2,25 @@ package com.alesat1215.productsfromerokhin.data
 
 import androidx.room.*
 
-data class Products(
-    var groups: List<Group>? = null
+data class RemoteData(
+    val title: String? = null,
+    val imageTitle: String? = null,
+    val listTitle: String? = null,
+    val listTitle2: String? = null,
+    val groups: List<Group>? = null
 ) {
     fun productsWithGroupOrder(): List<Product>? = groups?.flatMap { group ->
         group.products?.apply { map { it.groupOrder = group.order } } ?: emptyList()
     }
 }
 
-data class ProductsDB(
+data class Products(
     @Embedded val group: Group,
     @Relation(
         parentColumn = "order",
         entityColumn = "groupOrder"
     )
-    var products: List<Product>
+    val products: List<Product>
 )
 
 @Fts4
@@ -24,8 +28,8 @@ data class ProductsDB(
 data class Group(
     @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "rowid")
     var id: Int = 0,
-    var order: Int = 0,
-    var name: String? = null,
+    val order: Int = 0,
+    val name: String? = null,
     @Ignore var products: List<Product>? = null
 )
 
@@ -34,12 +38,12 @@ data class Group(
 data class Product(
     @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "rowid")
     var id: Int = 0,
-    var order: Int = 0,
+    val order: Int = 0,
     var groupOrder: Int = 0,
-    var name: String? = null,
-    var consist: String? = null,
-    var img: String? = null,
-    var price: Int? = null,
-    var inStart: Boolean = false,
-    var inStart2: Boolean = false
+    val name: String? = null,
+    val consist: String? = null,
+    val img: String? = null,
+    val price: Int? = null,
+    val inStart: Boolean = false,
+    val inStart2: Boolean = false
 )
