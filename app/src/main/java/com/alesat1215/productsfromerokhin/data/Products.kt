81393d6a8e4e7cb011f10049b7c9da79782721c4
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-06-04 23:03:02 +0300

    add room

--- app/src/main/java/com/alesat1215/productsfromerokhin/data/Products.kt
+++ app/src/main/java/com/alesat1215/productsfromerokhin/data/Products.kt
@@ -2,41 +2,43 @@ package com.alesat1215.productsfromerokhin.data
 
 import androidx.room.*
 
+@Fts4
+@Entity
 data class RemoteData(
     val title: String? = null,
     val imageTitle: String? = null,
     val listTitle: String? = null,
-    val listTitle2: String? = null,
-    val groups: List<Group>? = null
+    val listTitle2: String? = null
 ) {
-    fun productsWithGroupOrder(): List<Product>? = groups?.flatMap { group ->
+    @Ignore var groups: List<Group>? = null
+
+    fun productsWithGroupId(): List<Product>? = groups?.flatMap { group ->
         group.products?.apply { map { it.group = group.id } } ?: emptyList()
     }
 }
 
-data class Products(
-    @Embedded val group: Group,
-    @Relation(
-        parentColumn = "id",
-        entityColumn = "group"
-    )
-    val products: List<Product>
-)
+//data class Products(
+//    @Embedded val group: Group,
+//    @Relation(
+//        parentColumn = "id",
+//        entityColumn = "group"
+//    )
+//    val products: List<Product>
+//)
 
 @Fts4
 @Entity
 data class Group(
-    @PrimaryKey @ColumnInfo(name = "rowid")
-    var id: Int = 0,
-    val name: String? = null,
+    val id: Int = 0,
+    val name: String? = null
+) {
     @Ignore var products: List<Product>? = null
-)
+}
 
 @Fts4
 @Entity
 data class Product(
-    @PrimaryKey @ColumnInfo(name = "rowid")
-    var id: Int = 0,
+    val id: Int = 0,
     var group: Int = 0,
     val name: String? = null,
     val consist: String? = null,
---
+++ app/src/main/java/com/alesat1215/productsfromerokhin/data/ProductsDatabase.kt
@@ -0,0 +1,37 @@
+package com.alesat1215.productsfromerokhin.data
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface ProductsDao {
+    @Query("SELECT * FROM product")
+    fun products(): LiveData<List<Product>>
+    @Query("SELECT * FROM `group`")
+    fun groups(): LiveData<List<Group>>
+    @Query("SELECT * FROM remotedata LIMIT 1")
+    fun titles(): LiveData<RemoteData>
+
+    @Transaction
+    fun update(data: RemoteData) {
+        val products = data.productsWithGroupId()
+        if (products != null)
+            insertProducts(products)
+        val groups = data.groups
+        if (groups != null)
+            insertGroups(groups)
+        insertTitles(data)
+    }
+    @Insert(entity = Product::class, onConflict = OnConflictStrategy.REPLACE)
+    fun insertProducts(products: List<Product>)
+    @Insert(entity = Group::class, onConflict = OnConflictStrategy.REPLACE)
+    fun insertGroups(products: List<Group>)
+    @Insert(entity = RemoteData::class, onConflict = OnConflictStrategy.REPLACE)
+    fun insertTitles(data: RemoteData)
+}
+
+@Database(entities = [Product::class, Group::class, RemoteData::class], version = 1, exportSchema = false)
+abstract class ProductsDatabase: RoomDatabase() {
+    abstract fun productsDao(): ProductsDao
+}
+
