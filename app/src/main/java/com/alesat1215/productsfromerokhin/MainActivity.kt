0cf267095a4a764bf8d50d6fa73a3836c0ec6333
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-06-05 12:16:18 +0300

    add update db form fbDB

--- app/src/main/java/com/alesat1215/productsfromerokhin/MainActivity.kt
+++ app/src/main/java/com/alesat1215/productsfromerokhin/MainActivity.kt
@@ -2,62 +2,14 @@ package com.alesat1215.productsfromerokhin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import com.alesat1215.productsfromerokhin.data.RemoteData
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-    private val auth = FirebaseAuth.getInstance()
-    private val db = FirebaseDatabase.getInstance().reference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-        firebaseTest()
     }
 
-    private fun firebaseTest() {
-        auth.signInAnonymously().addOnCompleteListener {
-            if (it.isSuccessful) {
-                Log.d("firebase", auth.currentUser.toString())
-                db.addValueEventListener(object : ValueEventListener {
-                    override fun onCancelled(error: DatabaseError) {
-                        Log.d("firebase", error.message)
-                    }
-
-                    override fun onDataChange(snapshot: DataSnapshot) {
-                        val data = snapshot.getValue(RemoteData::class.java)
-                        Log.d("firebase", "${data?.title} ${data?.imageTitle} ${data?.listTitle} ${data?.listTitle2}")
-                        for (product in data?.productsWithGroupId() ?: emptyList()) {
-                            Log.d("firebase", "${product.group} ${product.id}")
-                        }
-                    }
-
-                })
-//                db.collection("groups").addSnapshotListener { snapshot, exception ->
-//                    if (exception != null) {
-//                        Log.d("firebase", exception.localizedMessage)
-//                        return@addSnapshotListener
-//                    }
-//                    if (snapshot != null) {
-//                        Log.d("firebase", "${snapshot.size()}")
-//                        val groups = snapshot.map { it.toObject(Group::class.java).apply { id = it.id } }
-//                        for (group in groups) {
-//                            Log.d("firebase", "${group.id} ${group.index} ${group.name}")
-//                        }
-//                        val products = groups.map {
-//                            db.collection("groups").document(it.id).collection("products")
-//                        }
-//                    }
-//                }
-            } else Log.d("firebase", "Login failed")
-        }
-    }
 }
--- app/src/main/java/com/alesat1215/productsfromerokhin/start/StartViewModel.kt
+++ app/src/main/java/com/alesat1215/productsfromerokhin/start/StartViewModel.kt
@@ -1,15 +1,17 @@
 package com.alesat1215.productsfromerokhin.start
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Transformations
-import androidx.lifecycle.ViewModel
 import com.alesat1215.productsfromerokhin.data.Product
 import com.alesat1215.productsfromerokhin.data.ProductsRepository
 import com.alesat1215.productsfromerokhin.data.StartTitle
 
-class StartViewModel: ViewModel() {
-    private var repository = ProductsRepository()
-    private var products = repository.products()
+class StartViewModel(application: Application) : AndroidViewModel(application) {
+    private val repository = ProductsRepository(application)
+    private val products = repository.products_
+    val groups = repository.groups
 
     fun title(forType: StartTitle) = repository.title(forType)
 
