9eed916e1dfd40d8e125374b982a3a4551669a57
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-06-05 20:00:57 +0300

    add updateDB_

--- app/src/main/java/com/alesat1215/productsfromerokhin/data/ProductsRepository.kt
+++ app/src/main/java/com/alesat1215/productsfromerokhin/data/ProductsRepository.kt
@@ -26,17 +26,17 @@ class ProductsRepository(private val application: Application) {
     private val titles by lazy { db.productsDao().titles() }
 
     fun products(): LiveData<List<Product>> {
-        updateDB()
+        updateDB_()
         return products
     }
 
     fun titles(): LiveData<RemoteData?> {
-        updateDB()
+        updateDB_()
         return titles
     }
 
     fun groups(): LiveData<List<Group>> {
-        updateDB()
+        updateDB_()
         return groups
     }
 
@@ -73,6 +73,56 @@ class ProductsRepository(private val application: Application) {
             } else {
                 dbFBFetchLimit.reset()
                 Log.d("firebase", "Login failed")
+            }
+        }
+    }
+
+    private fun updateDB_() {
+        if(!dbFBFetchLimit.shouldFetch()) return
+        signInFB {
+            dbFB.database.goOnline()
+            dbFB.addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onCancelled(error: DatabaseError) {
+                    dbFBFetchLimit.reset()
+                    dbFB.database.goOffline()
+                    Log.d("Firebase", error.message)
+                }
+
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    val data = snapshot.getValue(RemoteData::class.java)
+                    Log.d("Firebase", "${data?.title} ${data?.imageTitle} ${data?.listTitle} ${data?.listTitle2}")
+                    for (product in data?.productsWithGroupId() ?: emptyList()) {
+                        Log.d("Firebase", "${product.group} ${product.id}")
+                    }
+                    if (data != null) {
+                        GlobalScope.launch(Dispatchers.IO) {
+                            db.withTransaction {
+                                db.clearAllTables()
+                                db.productsDao().update(data)
+                                Log.d("Firebase", "db is updated")
+                            }
+                        }
+                    }
+                    dbFB.database.goOffline()
+                }
+
+            })
+        }
+    }
+
+    private fun signInFB(onSuccess: () -> Unit) {
+        if (auth.currentUser != null) {
+            Log.d("Firebase", "Already sign in: ${auth.currentUser}")
+            onSuccess()
+            return
+        }
+        auth.signInAnonymously().addOnCompleteListener {
+            if (it.isSuccessful) {
+                Log.d("Firebase", "Sign in SUCCESS: ${auth.currentUser}")
+                onSuccess()
+            } else {
+                dbFBFetchLimit.reset()
+                Log.d("Firebase", "Sign in FAILED: ${it.exception}")
             }
         }
     }
