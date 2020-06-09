a8585c9a7684d7b3da4b286df726305030480e3f
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-06-09 14:38:25 +0300

    list to products

--- app/src/main/java/com/alesat1215/productsfromerokhin/start/StartViewModel.kt
+++ app/src/main/java/com/alesat1215/productsfromerokhin/start/StartViewModel.kt
@@ -18,8 +18,8 @@ class StartViewModel @Inject constructor(
         when (forType) {
             StartTitle.TITLE -> Transformations.map(repository.titles()) { it?.title }
             StartTitle.IMAGE -> Transformations.map(repository.titles()) { it?.imageTitle }
-            StartTitle.LIST -> Transformations.map(repository.titles()) { it?.listTitle }
-            StartTitle.LIST2 -> Transformations.map(repository.titles()) { it?.listTitle2 }
+            StartTitle.PRODUCTS -> Transformations.map(repository.titles()) { it?.listTitle }
+            StartTitle.PRODUCTS2 -> Transformations.map(repository.titles()) { it?.listTitle2 }
         }
 
     fun products(predicate: ((Product) -> Boolean)? = null): LiveData<List<Product>> {
