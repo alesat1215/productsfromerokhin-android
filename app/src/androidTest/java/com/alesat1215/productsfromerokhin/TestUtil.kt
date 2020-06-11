0a43078511b038b8df220b2d02ed70296943c521
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-06-11 19:38:43 +0300

    refactor test util

--- app/src/androidTest/java/com/alesat1215/productsfromerokhin/TestUtil.kt
+++ app/src/androidTest/java/com/alesat1215/productsfromerokhin/TestUtil.kt
@@ -4,16 +4,16 @@ import com.alesat1215.productsfromerokhin.data.Group
 import com.alesat1215.productsfromerokhin.data.Product
 import com.alesat1215.productsfromerokhin.data.RemoteData
 
-fun remoteDataMock() = RemoteData(
+fun remoteDataMockAndroidTest() = RemoteData(
     "Title",
     "Img",
     "ImgTitle",
     "ProductsTitle",
     "ProductsTitle2"
 ).apply { groups = listOf(
-    Group(1).apply { products = products123() },
-    Group(2).apply { products = products456() }
+    Group(1).apply { products = products123AndroidTest() },
+    Group(2).apply { products = products456AndroidTest() }
 ) }
 
-fun products123() = listOf(Product(1), Product(2), Product(3))
-fun products456() = listOf(Product(4), Product(5), Product(6))
+fun products123AndroidTest() = listOf(Product(1), Product(2), Product(3))
+fun products456AndroidTest() = listOf(Product(4), Product(5), Product(6))
