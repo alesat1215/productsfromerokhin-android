df6d2f8c95d3e21b60af31fe3cd09144c72b1c7a
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-06-12 18:46:48 +0300

    del comments

--- app/src/androidTest/java/com/alesat1215/productsfromerokhin/MainActivityTest.kt
+++ app/src/androidTest/java/com/alesat1215/productsfromerokhin/MainActivityTest.kt
@@ -6,8 +6,6 @@ import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import androidx.test.rule.ActivityTestRule
-import org.hamcrest.Matchers.allOf
-import org.hamcrest.core.AnyOf.anyOf
 import org.junit.Assert.*
 import org.junit.Rule
 import org.junit.Test
