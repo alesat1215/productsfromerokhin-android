63632a81918ed2b9f1288cb2a561705a3b0f98ac
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-06-12 18:15:54 +0300

    add checkViews

---
+++ app/src/androidTest/java/com/alesat1215/productsfromerokhin/MainActivityTest.kt
@@ -0,0 +1,28 @@
+package com.alesat1215.productsfromerokhin
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.core.AnyOf.anyOf
+import org.junit.Assert.*
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class MainActivityTest {
+    @get:Rule
+    val activityRule = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun checkViews() {
+        onView(withId(R.id.toolbar)).check(matches(isDisplayed()))
+        onView(withId(R.id.nav_view)).check(matches(isDisplayed()))
+        onView(withId(R.id.nav_host_fragment)).check(matches(isDisplayed()))
+    }
+}
