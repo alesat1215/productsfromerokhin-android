b9818eb9cecc410f566ef4988c51783c30891921
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-06-14 19:14:27 +0300

    refactor StartFragmentTest

--- app/src/androidTest/java/com/alesat1215/productsfromerokhin/start/StartFragmentTest.kt
+++ app/src/androidTest/java/com/alesat1215/productsfromerokhin/start/StartFragmentTest.kt
@@ -4,10 +4,11 @@ import android.graphics.ColorSpace.match
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
 import com.alesat1215.productsfromerokhin.R
+import com.alesat1215.productsfromerokhin.remoteDataMockAndroidTest
 import org.junit.Before
 
 import org.junit.Assert.*
@@ -15,17 +16,21 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import java.lang.Thread.sleep
 
+@LargeTest
 @RunWith(AndroidJUnit4::class)
 class StartFragmentTest {
+    private val data by lazy { remoteDataMockAndroidTest() }
 
     @Before
     fun setUp() {
-        val scenario = launchFragmentInContainer<StartFragment>(themeResId = R.style.AppTheme)
+        launchFragmentInContainer<StartFragment>(themeResId = R.style.AppTheme)
     }
 
     @Test
     fun checkViews() {
-        sleep(30000)
-        onView(withId(R.id.title)).check(matches(isDisplayed()))
+        onView(withId(R.id.title)).check(matches(withText(data.title)))
+        onView(withId(R.id.imgTitle)).check(matches(withText(data.imgTitle)))
+        onView(withId(R.id.productsTitle)).check(matches(withText(data.productsTitle)))
+        onView(withId(R.id.productsTitle2)).check(matches(withText(data.productsTitle2)))
     }
 }
