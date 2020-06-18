ab014a35926f2d34ed7041e11de2e840c9ec4192
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-06-18 18:11:38 +0400

    add tabs to MenuFragment

--- app/src/main/java/com/alesat1215/productsfromerokhin/menu/MenuFragment.kt
+++ app/src/main/java/com/alesat1215/productsfromerokhin/menu/MenuFragment.kt
@@ -6,9 +6,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.alesat1215.productsfromerokhin.R
+import com.alesat1215.productsfromerokhin.databinding.FragmentMenuBinding
 import com.alesat1215.productsfromerokhin.start.StartViewModel
+import com.google.android.material.tabs.TabLayout
 import dagger.android.support.DaggerFragment
 import javax.inject.Inject
 
@@ -24,9 +27,16 @@ class MenuFragment : DaggerFragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_menu, container, false)
+    ) = FragmentMenuBinding.inflate(inflater, container, false).apply {
+        lifecycleOwner = this@MenuFragment
+        groupsToTabs(groups)
+    }.root
+
+    private fun groupsToTabs(tabs: TabLayout) {
+        viewModel.groups().observe(viewLifecycleOwner, Observer {
+            tabs.removeAllTabs()
+            it.forEach { tabs.addTab(tabs.newTab().setText(it.name)) }
+        })
     }
 
 }
