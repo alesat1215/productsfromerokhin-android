980419e1b24b67dab63ec56d43cfdadfba219929
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-06-18 18:45:08 +0400

    add products to MenuFragment

--- app/src/main/java/com/alesat1215/productsfromerokhin/menu/MenuFragment.kt
+++ app/src/main/java/com/alesat1215/productsfromerokhin/menu/MenuFragment.kt
@@ -3,16 +3,17 @@ package com.alesat1215.productsfromerokhin.menu
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 import com.alesat1215.productsfromerokhin.R
 import com.alesat1215.productsfromerokhin.databinding.FragmentMenuBinding
-import com.alesat1215.productsfromerokhin.start.StartViewModel
+import com.alesat1215.productsfromerokhin.util.BindRVAdapter
 import com.google.android.material.tabs.TabLayout
 import dagger.android.support.DaggerFragment
+import kotlinx.android.synthetic.main.fragment_menu.*
 import javax.inject.Inject
 
 /**
@@ -28,8 +29,10 @@ class MenuFragment : DaggerFragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ) = FragmentMenuBinding.inflate(inflater, container, false).apply {
-        lifecycleOwner = this@MenuFragment
         groupsToTabs(groups)
+        adapterToProducts(productsMenu)
+        lifecycleOwner = this@MenuFragment
+        executePendingBindings()
     }.root
 
     private fun groupsToTabs(tabs: TabLayout) {
@@ -38,5 +41,11 @@ class MenuFragment : DaggerFragment() {
             it.forEach { tabs.addTab(tabs.newTab().setText(it.name)) }
         })
     }
+
+    /** Set adapter for list with predicate for dataSet */
+    private fun adapterToProducts(list: RecyclerView) =
+        viewModel.products().observe(viewLifecycleOwner, Observer {
+            list.swapAdapter(BindRVAdapter(it, R.layout.product_item), false)
+        })
 
 }
