4e7d3f22e274d7198c1cff41de688f79d87b1ac4
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-06-18 21:29:43 +0400

    refactor switchGroup

--- app/src/main/java/com/alesat1215/productsfromerokhin/menu/MenuFragment.kt
+++ app/src/main/java/com/alesat1215/productsfromerokhin/menu/MenuFragment.kt
@@ -1,19 +1,25 @@
 package com.alesat1215.productsfromerokhin.menu
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.children
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.alesat1215.productsfromerokhin.R
+import com.alesat1215.productsfromerokhin.data.Product
 import com.alesat1215.productsfromerokhin.databinding.FragmentMenuBinding
 import com.alesat1215.productsfromerokhin.util.BindRVAdapter
 import com.google.android.material.tabs.TabLayout
 import dagger.android.support.DaggerFragment
 import kotlinx.android.synthetic.main.fragment_menu.*
+import java.util.function.BiPredicate
 import javax.inject.Inject
 
 /**
@@ -25,12 +31,15 @@ class MenuFragment : DaggerFragment() {
 
     private val viewModel by viewModels<MenuViewModel> { viewModelFactory }
 
+    private val groupTabs = mutableListOf<TabLayout.Tab>()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ) = FragmentMenuBinding.inflate(inflater, container, false).apply {
         groupsToTabs(groups)
         adapterToProducts(productsMenu)
+        switchGroup(productsMenu)
         lifecycleOwner = this@MenuFragment
         executePendingBindings()
     }.root
@@ -38,7 +47,15 @@ class MenuFragment : DaggerFragment() {
     private fun groupsToTabs(tabs: TabLayout) {
         viewModel.groups().observe(viewLifecycleOwner, Observer {
             tabs.removeAllTabs()
-            it.forEach { tabs.addTab(tabs.newTab().setText(it.name)) }
+            groupTabs.clear()
+            it.forEach {
+                val tab = tabs.newTab().apply {
+                    text = it.name
+                    tag = it.id
+                }
+                groupTabs.add(tab)
+                tabs.addTab(tab)
+            }
         })
     }
 
@@ -47,5 +64,20 @@ class MenuFragment : DaggerFragment() {
         viewModel.products().observe(viewLifecycleOwner, Observer {
             list.swapAdapter(BindRVAdapter(it, R.layout.product_item), false)
         })
+
+    private fun switchGroup(list: RecyclerView) {
+        list.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+
+                val index = (recyclerView.layoutManager as? LinearLayoutManager)?.findLastCompletelyVisibleItemPosition()
+                val product = (recyclerView.adapter as? BindRVAdapter<Product>)?.itemAtIndex(index)
+                val group = groups.getTabAt(groups.selectedTabPosition)
+                if (group?.tag != product?.group)  {
+                    groupTabs.filter { it.tag == product?.group }.firstOrNull()?.select()
+                }
+            }
+        })
+    }
 
 }
