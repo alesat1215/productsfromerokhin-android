c0e52554801a4a8f9d31e9637a49d9a58ce2fba4
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-06-21 22:43:30 +0400

    dell imports

--- app/src/androidTest/java/com/alesat1215/productsfromerokhin/MainActivityTest.kt
+++ app/src/androidTest/java/com/alesat1215/productsfromerokhin/MainActivityTest.kt
@@ -7,12 +7,9 @@ import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import androidx.test.rule.ActivityTestRule
-import org.hamcrest.Matchers.allOf
-import org.junit.Assert.*
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import java.lang.Thread.sleep
 
 @RunWith(AndroidJUnit4::class)
 @LargeTest
