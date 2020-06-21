8140315ab9ea30b57f9dcca79c4826ff21616beb
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-06-21 20:56:06 +0400

    refactor MenuFragmentTest

--- app/src/androidTest/java/com/alesat1215/productsfromerokhin/menu/MenuFragmentTest.kt
+++ app/src/androidTest/java/com/alesat1215/productsfromerokhin/menu/MenuFragmentTest.kt
@@ -2,18 +2,23 @@ package com.alesat1215.productsfromerokhin.menu
 
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import com.alesat1215.productsfromerokhin.R
+import com.alesat1215.productsfromerokhin.products456AndroidTest
 import com.alesat1215.productsfromerokhin.remoteDataMockAndroidTest
+import com.alesat1215.productsfromerokhin.util.BindViewHolder
+import org.hamcrest.CoreMatchers.not
 import org.junit.Before
 
 import org.junit.Assert.*
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.lang.Thread.sleep
 
 @LargeTest
 @RunWith(AndroidJUnit4::class)
@@ -29,5 +34,18 @@ class MenuFragmentTest {
     fun checkViews() {
         onView(withId(R.id.groups)).check(matches(isDisplayed()))
         onView(withId(R.id.products_menu)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun switchGroup() {
+//        onView(withId(R.id.products_menu))
+//            .perform(RecyclerViewActions.scrollToPosition<BindViewHolder>(data.productsWithGroupId()!!.count() - 1))
+//        onView(withId(R.id.groups)).check()
+//        sleep(10000)
+//        onView(withText(products456AndroidTest().first().name)).check(matches(not(isDisplayed())))
+        sleep(10000)
+        onView(withText(data.groups!!.last().name)).perform(click())
+        sleep(10000)
+        onView(withText(products456AndroidTest().first().name)).check(matches(isDisplayed()))
     }
 }
