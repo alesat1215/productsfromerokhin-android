b59314c32a553acf7b4fccc5240a2c6ff8fbd035
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-07-10 16:39:13 +0400

    add comments

--- app/src/main/java/com/alesat1215/productsfromerokhin/menu/MenuFragment.kt
+++ app/src/main/java/com/alesat1215/productsfromerokhin/menu/MenuFragment.kt
@@ -2,7 +2,6 @@ package com.alesat1215.productsfromerokhin.menu
 
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
