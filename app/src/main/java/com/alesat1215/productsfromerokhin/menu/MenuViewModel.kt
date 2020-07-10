dbb8e76828353f5387791af888fabc291e1c496e
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-07-10 19:07:21 +0400

    refactor MenuViewModel

--- app/src/main/java/com/alesat1215/productsfromerokhin/menu/MenuViewModel.kt
+++ app/src/main/java/com/alesat1215/productsfromerokhin/menu/MenuViewModel.kt
@@ -1,6 +1,5 @@
 package com.alesat1215.productsfromerokhin.menu
 
-import android.os.Parcelable
 import androidx.lifecycle.ViewModel
 import com.alesat1215.productsfromerokhin.data.IProductsRepository
 import javax.inject.Inject
@@ -9,10 +8,7 @@ class MenuViewModel @Inject constructor(
     private val repository: IProductsRepository
 ) : ViewModel() {
 
-    /** Save state for list to remember scroll position */
-//    val recyclerViewState = mutableMapOf<Int, Parcelable>()
-//
-//    var selectedTabPosition = 0
+    /** Scroll position for products_menu */
     var scrollPosition = 0
 
     fun products() = repository.products()
