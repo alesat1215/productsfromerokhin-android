fce70f8600f421c8e63ee8c85e507d97274c9869
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-07-10 16:37:46 +0400

    add comments

--- app/src/main/java/com/alesat1215/productsfromerokhin/menu/MenuFragment.kt
+++ app/src/main/java/com/alesat1215/productsfromerokhin/menu/MenuFragment.kt
@@ -6,7 +6,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
-import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -15,7 +14,6 @@ import com.alesat1215.productsfromerokhin.R
 import com.alesat1215.productsfromerokhin.data.Group
 import com.alesat1215.productsfromerokhin.data.Product
 import com.alesat1215.productsfromerokhin.databinding.FragmentMenuBinding
-import com.alesat1215.productsfromerokhin.start.StartTitle
 import com.alesat1215.productsfromerokhin.util.BindRVAdapter
 import com.google.android.material.tabs.TabLayout
 import dagger.android.support.DaggerFragment
@@ -23,7 +21,8 @@ import kotlinx.android.synthetic.main.fragment_menu.*
 import javax.inject.Inject
 
 /**
- * A simple [Fragment] subclass.
+ * [MenuFragment] subclass of [DaggerFragment].
+ * Screen with groups & products
  */
 class MenuFragment : DaggerFragment() {
     @Inject
