ddb3f3ce9ab818562ea8d57eb672ee865080f74b
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-07-11 14:50:45 +0400

    refactor onCreateView

--- app/src/main/java/com/alesat1215/productsfromerokhin/load/LoadFragment.kt
+++ app/src/main/java/com/alesat1215/productsfromerokhin/load/LoadFragment.kt
@@ -26,10 +26,7 @@ class LoadFragment : DaggerFragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_load, container, false)
-    }
+    ) = inflater.inflate(R.layout.fragment_load, container, false)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
