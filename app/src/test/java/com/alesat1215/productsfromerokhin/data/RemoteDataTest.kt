0ec350e4eebe80aaad0fcd054a3fa21002f2c51a
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-07-13 18:34:35 +0400

    del comment

--- app/src/test/java/com/alesat1215/productsfromerokhin/data/RemoteDataTest.kt
+++ app/src/test/java/com/alesat1215/productsfromerokhin/data/RemoteDataTest.kt
@@ -68,28 +68,4 @@ class RemoteDataTest {
         assertEquals(product.inStart2, productDB.inStart2)
         assertEquals(group, productDB.group)
     }
-
-
-//    @Test
-//    fun productsWithGroupId() {
-//        val products = listOf(Product(id = 1), Product(id = 2), Product(id = 3))
-//        val products2 = listOf(Product(id = 4), Product(id = 5), Product(id = 6))
-//        val groups = listOf(
-//            Group(id = 1).apply { this.products = products },
-//            Group(id = 2).apply { this.products = products2 }
-//        )
-//        val data = RemoteData()//.apply { this.groups = groups }
-//        var result = data.productsWithGroupId()
-//        assertNull(result)
-//        data.groups = groups
-//        result = data.productsWithGroupId()
-//        assertNotNull(result)
-//        assertEquals(result?.count(), products.count() + products2.count())
-//        var group = groups.first()
-//        var productsGroup = result!!.take(group.products!!.count())
-//        productsGroup.forEach { assertEquals(it.group, group.id) }
-//        group = groups.last()
-//        productsGroup = result.takeLast(group.products!!.count())
-//        productsGroup.forEach { assertEquals(it.group, group.id) }
-//    }
 }
