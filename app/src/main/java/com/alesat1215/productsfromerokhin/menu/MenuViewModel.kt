fb25c6a1b2dd6b2230ba29d0854ff9fbd8f72850
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-07-18 13:54:42 +0300

    del import

--- app/src/main/java/com/alesat1215/productsfromerokhin/menu/MenuViewModel.kt
+++ app/src/main/java/com/alesat1215/productsfromerokhin/menu/MenuViewModel.kt
@@ -1,7 +1,6 @@
 package com.alesat1215.productsfromerokhin.menu
 
 import android.util.Log
-import android.widget.Toast
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.alesat1215.productsfromerokhin.data.IProductsRepository
