757097428e043901f64c738e3e0b1aa722b15c13
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-07-18 15:48:04 +0300

    add CartManager.kt

--- app/src/main/java/com/alesat1215/productsfromerokhin/menu/MenuViewModel.kt
+++ app/src/main/java/com/alesat1215/productsfromerokhin/menu/MenuViewModel.kt
@@ -1,16 +1,12 @@
 package com.alesat1215.productsfromerokhin.menu
 
-import android.util.Log
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.alesat1215.productsfromerokhin.data.IProductsRepository
-import com.alesat1215.productsfromerokhin.data.local.Product
-import kotlinx.coroutines.launch
+import com.alesat1215.productsfromerokhin.util.CartManager
 import javax.inject.Inject
 
 class MenuViewModel @Inject constructor(
-    private val repository: IProductsRepository
-) : ViewModel() {
+    override val repository: IProductsRepository
+) : CartManager() {
 
     /** Scroll position for products_menu */
     var scrollPosition = 0
@@ -18,20 +14,4 @@ class MenuViewModel @Inject constructor(
     fun products() = repository.products()
 
     fun groups() = repository.groups()
-
-    fun addProductToCart(product: Product) {
-        viewModelScope.launch {
-            repository.addProductToCart(product.productInCart())
-            Log.d("Menu", "Add to cart: ${product.productDB?.name}")
-        }
-    }
-
-    fun delProductFromCart(product: Product) {
-        viewModelScope.launch {
-            product.inCart.firstOrNull()?.also {
-                repository.delProductFromCart(it)
-                Log.d("Menu", "Del from cart: ${it.name}")
-            }
-        }
-    }
 }
