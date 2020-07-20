9f62f1c4d0eac91b7e7c5ebfe4cd09621722e975
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-07-20 17:22:27 +0300

    add search contact

--- app/src/main/java/com/alesat1215/productsfromerokhin/cart/CartFragment.kt
+++ app/src/main/java/com/alesat1215/productsfromerokhin/cart/CartFragment.kt
@@ -3,9 +3,11 @@ package com.alesat1215.productsfromerokhin.cart
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -14,6 +16,8 @@ import com.alesat1215.productsfromerokhin.data.local.Product
 import com.alesat1215.productsfromerokhin.databinding.FragmentCartBinding
 import com.alesat1215.productsfromerokhin.util.BindRVAdapter
 import dagger.android.support.DaggerFragment
+import android.Manifest
+import android.content.pm.PackageManager
 import javax.inject.Inject
 
 /**
@@ -57,17 +61,34 @@ class CartFragment : DaggerFragment() {
 //            putExtra(Intent.EXTRA_TEXT, "textMessage")
 //            type = "text/plain"
 //        }
-        val intent = Intent().apply {
-            action = Intent.ACTION_VIEW
-            `package` = "com.whatsapp"
-            data = Uri.parse("https://api.whatsapp.com/send?phone=79021228236&text=textMessage")
-        }
-        val chooser: Intent = Intent.createChooser(intent, "title")
-        activity?.packageManager?.also {
-            intent.resolveActivity(it)?.also {
-                startActivity(chooser)
-                Log.d("Cart", "Confirm")
+
+
+        if (activity?.checkSelfPermission(Manifest.permission.READ_CONTACTS) == PackageManager.PERMISSION_GRANTED) {
+            searchPhoneInContacts()
+            Log.d("Cart", "PERMISSION_GRANTED")
+        } else {
+            if (shouldShowRequestPermissionRationale(Manifest.permission.READ_CONTACTS)) {
+                requestPermissions(arrayOf(Manifest.permission.READ_CONTACTS), 0)
+                Log.d("Cart", "PERMISSION rationale true")
+            } else {
+                requestPermissions(arrayOf(Manifest.permission.READ_CONTACTS), 0)
+                Log.d("Cart", "PERMISSION rationale false")
             }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 0) {
+            if (grantResults.firstOrNull() == PackageManager.PERMISSION_GRANTED) {
+                Log.d("Cart", "PERMISSION_GRANTED")
+                searchPhoneInContacts()
+            }
+            else Log.d("Cart", "PERMISSION_DENIED")
         }
     }
 
@@ -83,6 +104,17 @@ class CartFragment : DaggerFragment() {
                 startActivity(chooser)
                 Log.d("Cart", "Confirm")
             }
+        }
+    }
+
+    private fun searchPhoneInContacts() {
+        val uri = Uri.withAppendedPath(ContactsContract.Contacts.CONTENT_FILTER_URI, Uri.encode("+79021228236"))
+        val projection = arrayOf(ContactsContract.Contacts.DISPLAY_NAME)
+        context?.contentResolver?.query(uri, projection, null, null, null)?.also {
+            if (it.moveToFirst()) {
+                Log.d("Cart", "Contact: ${it.getString(0)}")
+            } else Log.d("Cart", "Contact not found")
+            it.close()
         }
     }
 
