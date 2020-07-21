4991ee8776f73a46ed6f676a3d5836890d1e11bd
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-07-21 15:19:42 +0300

    del button to bar

--- app/src/main/java/com/alesat1215/productsfromerokhin/MainActivity.kt
+++ app/src/main/java/com/alesat1215/productsfromerokhin/MainActivity.kt
@@ -2,6 +2,9 @@ package com.alesat1215.productsfromerokhin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import kotlinx.android.synthetic.main.activity_main.*
@@ -13,6 +16,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
         nav_view.setupWithNavController(findNavController(R.id.nav_host_fragment))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+//        return super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.toollbar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        return super.onOptionsItemSelected(item)
+        if (item.itemId == R.id.clearCart) {
+            Log.d("Cart", "Del button")
+            return true
+        } else return super.onOptionsItemSelected(item)
     }
 
 }
