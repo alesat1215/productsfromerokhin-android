8ad51c5b3489e3e99756f7decfd2d0407976b855
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-07-24 15:33:46 +0300

    add CartFragmentTest

---
+++ app/src/androidTest/java/com/alesat1215/productsfromerokhin/cart/CartFragmentTest.kt
@@ -0,0 +1,41 @@
+package com.alesat1215.productsfromerokhin.cart
+
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import com.alesat1215.productsfromerokhin.R
+import org.junit.Before
+import org.junit.Test
+
+import org.junit.Assert.*
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class CartFragmentTest {
+
+    @Before
+    fun setUp() {
+        launchFragmentInContainer<CartFragment>(themeResId = R.style.AppTheme)
+    }
+
+    @Test
+    fun checkViews() {
+        onView(withId(R.id.products_cart))
+            .check(matches(isDisplayed()))
+        onView(withId(R.id.result_text))
+            .check(matches(isDisplayed()))
+        onView(withId(R.id.result_sum))
+            .check(matches(isDisplayed()))
+        onView(withId(R.id.send_cart))
+            .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun send() {
+    }
+}
