4a32c1074dcfae5a8b9f577876c06142f856edcb
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-07-24 19:17:42 +0300

    add instructions to db

--- app/src/main/java/com/alesat1215/productsfromerokhin/data/Tutorial.kt
+++ app/src/main/java/com/alesat1215/productsfromerokhin/data/Tutorial.kt
@@ -1,9 +1,16 @@
 package com.alesat1215.productsfromerokhin.data
 
+import androidx.room.Entity
+import androidx.room.Fts4
+import com.alesat1215.productsfromerokhin.data.local.GroupDB
+
 data class Tutorial(
     val instructions: List<Instruction> = emptyList()
 )
 
+/** Model for [Instruction] */
+@Fts4
+@Entity
 data class Instruction(
     val text: String = "",
     val img_path: String = ""
