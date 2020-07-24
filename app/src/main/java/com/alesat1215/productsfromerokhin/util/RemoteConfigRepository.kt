45a9a888db7c74f41acc7991991d9b4219467b11
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-07-24 21:28:59 +0300

    add comments

--- app/src/main/java/com/alesat1215/productsfromerokhin/util/RemoteConfigRepository.kt
+++ app/src/main/java/com/alesat1215/productsfromerokhin/util/RemoteConfigRepository.kt
@@ -3,9 +3,10 @@ package com.alesat1215.productsfromerokhin.util
 import android.util.Log
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig
 
+/** Fetch Firebase remote config */
 interface RemoteConfigRepository {
     val remoteConfig: FirebaseRemoteConfig
-
+    /** Fetch & activate remote config & exec onSuccess() */
     fun fetchAndActivate(onSuccess: () -> Unit) {
         remoteConfig.fetchAndActivate().addOnCompleteListener {
             if (it.isSuccessful) {
