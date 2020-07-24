3ea610b531a80db1793b14b887ac6278199b2326
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-07-24 11:53:22 +0300

    del import

--- app/src/main/java/com/alesat1215/productsfromerokhin/data/local/Profile.kt
+++ app/src/main/java/com/alesat1215/productsfromerokhin/data/local/Profile.kt
@@ -2,7 +2,6 @@ package com.alesat1215.productsfromerokhin.data.local
 
 import androidx.room.Entity
 import androidx.room.Fts4
-import androidx.room.Ignore
 import androidx.room.PrimaryKey
 
 /** Model for [Profile] data */
