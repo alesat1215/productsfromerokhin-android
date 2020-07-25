bdc1b1a1f65aa94b997f0431093a9a9fc798dfc7
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-07-25 16:56:03 +0300

    add pager for tutorial

--- app/src/main/java/com/alesat1215/productsfromerokhin/tutorial/TutorialFragment.kt
+++ app/src/main/java/com/alesat1215/productsfromerokhin/tutorial/TutorialFragment.kt
@@ -5,11 +5,17 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
 import com.alesat1215.productsfromerokhin.R
+import com.alesat1215.productsfromerokhin.data.Instruction
 import dagger.android.support.DaggerFragment
+import kotlinx.android.synthetic.main.fragment_instruction.*
+import kotlinx.android.synthetic.main.fragment_tutorial.*
 import javax.inject.Inject
 
 /**
@@ -26,14 +32,58 @@ class TutorialFragment : DaggerFragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        checkRemoteConfig()
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_tutorial, container, false)
     }
 
-    private fun checkRemoteConfig() {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+//        setAdapterToPager(pager_tutorial)
+        pager_tutorial.adapter = TutorialAdapter(this)
+        setDataToPagerAdapter()
+    }
+
+    private fun setDataToPagerAdapter() {
         viewModel.instructions().observe(viewLifecycleOwner, Observer {
             Log.d("Tutorial", "${it}")
+//            pager.adapter = TutorialAdapter(this, it)
+            (pager_tutorial.adapter as? TutorialAdapter)?.apply {
+                instructions = it
+                notifyDataSetChanged()
+            }
         })
+    }
+}
+
+class TutorialAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
+    var instructions: List<Instruction> = emptyList()
+
+    override fun getItemCount() = instructions.count()
+
+    override fun createFragment(position: Int): Fragment {
+         return InstructionFragment().apply {
+            arguments = Bundle().apply { putParcelable(SERIALIZABLE_KEY, instructions[position]) }
+        }
+    }
+
+}
+
+private const val SERIALIZABLE_KEY = "SERIALIZABLE_KEY"
+
+class InstructionFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+//        return super.onCreateView(inflater, container, savedInstanceState)
+        return inflater.inflate(R.layout.fragment_instruction, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        arguments?.takeIf { it.containsKey(SERIALIZABLE_KEY) }?.apply {
+            text_instruction.text = getParcelable<Instruction>(SERIALIZABLE_KEY)?.text
+        }
     }
 }
--- app/src/main/java/com/alesat1215/productsfromerokhin/tutorial/TutorialViewModel.kt
+++ app/src/main/java/com/alesat1215/productsfromerokhin/tutorial/TutorialViewModel.kt
@@ -10,6 +10,5 @@ import javax.inject.Inject
 class TutorialViewModel @Inject constructor(
     private val repository: ITutorialRepository
 ) : ViewModel() {
-    var step = 0
     fun instructions() = repository.instructions()
 }
