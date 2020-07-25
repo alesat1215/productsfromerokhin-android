c62e14dbb57a6c7ac8ec20e0a8098f1cd70dabc4
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-07-25 19:08:43 +0300

    InstructionFragment to binding

--- app/src/main/java/com/alesat1215/productsfromerokhin/tutorial/InstructionFragment.kt
+++ app/src/main/java/com/alesat1215/productsfromerokhin/tutorial/InstructionFragment.kt
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.alesat1215.productsfromerokhin.R
 import com.alesat1215.productsfromerokhin.data.Instruction
+import com.alesat1215.productsfromerokhin.databinding.FragmentInstructionBinding
 import com.alesat1215.productsfromerokhin.util.ViewPagerAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_instruction.*
@@ -22,26 +23,18 @@ class InstructionFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-//        return super.onCreateView(inflater, container, savedInstanceState)
-        return inflater.inflate(R.layout.fragment_instruction, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
+    ) = FragmentInstructionBinding.inflate(inflater, container, false).apply {
+        // Set text
         arguments?.takeIf { it.containsKey(ViewPagerAdapter.ITEM) }?.apply {
-            text_instruction.text = getParcelable<Instruction>(ViewPagerAdapter.ITEM)?.text
+            textInstruction.text = getParcelable<Instruction>(ViewPagerAdapter.ITEM)?.text
         }
+        // Set OK button visibility
         arguments?.takeIf { it.containsKey(ViewPagerAdapter.IS_LAST) }?.apply {
             val isLast = getBoolean(ViewPagerAdapter.IS_LAST)
             Log.d("Tutorial", "Last instruction: ${getBoolean(ViewPagerAdapter.IS_LAST)}")
-            ok_instruction.visibility = if (isLast) View.VISIBLE else View.INVISIBLE
+            okInstruction.visibility = if (isLast) View.VISIBLE else View.INVISIBLE
         }
-        ok_instruction.setOnClickListener {
-            findNavController().navigate(R.id.action_tutorialFragment_to_startFragment)
-        }
-    }
+    }.root
 
     override fun onStart() {
         super.onStart()
