df81242b687d95a812d242bc72109299b7698c45
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-07-26 15:20:35 +0300

    add RemoteConfigKeys

---
+++ app/src/main/java/com/alesat1215/productsfromerokhin/data/RemoteConfigKeys.kt
@@ -0,0 +1,7 @@
+package com.alesat1215.productsfromerokhin.data
+
+object RemoteConfigKeys {
+    const val TITLES = "titles"
+    const val PRODUCTS = "products"
+    const val INSTRUCTIONS = "instructions"
+}
