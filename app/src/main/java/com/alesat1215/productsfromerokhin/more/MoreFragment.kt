06016d45bd66264d95ca998eeda78274f9906ab9
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-07-26 13:50:39 +0300

    more to scroll

--- app/src/main/java/com/alesat1215/productsfromerokhin/more/MoreFragment.kt
+++ app/src/main/java/com/alesat1215/productsfromerokhin/more/MoreFragment.kt
@@ -3,8 +3,10 @@ package com.alesat1215.productsfromerokhin.more
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import com.alesat1215.productsfromerokhin.databinding.FragmentMoreBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 /**
  * [MoreFragment] subclass of [Fragment].
@@ -16,4 +18,10 @@ class MoreFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ) = FragmentMoreBinding.inflate(inflater, container, false).root
+
+    override fun onStart() {
+        super.onStart()
+        // Show BottomNavigationView
+        activity?.nav_view?.visibility = View.VISIBLE
+    }
 }
