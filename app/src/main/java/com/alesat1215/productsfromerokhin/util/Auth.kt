c8ca1531fc834c41f6079a8649619172026a2e5a
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-07-28 18:53:35 +0300

    add auth

--- app/src/main/java/com/alesat1215/productsfromerokhin/util/Auth.kt
+++ app/src/main/java/com/alesat1215/productsfromerokhin/util/Auth.kt
@@ -12,29 +12,24 @@ import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class Auth @Inject constructor(private val auth: FirebaseAuth) {
-    private val result = MutableLiveData<Result<Unit>>()
+class Auth @Inject constructor(
+    private val auth: FirebaseAuth,
+    private val authComplete: AuthComplete
+) {
 
     fun signIn(): LiveData<Result<Unit>> {
         if (auth.currentUser != null) {
             Logger.d("Already sign in: ${auth.currentUser}")
-            result.value = Result.success(Unit)
+            return MutableLiveData(Result.success(Unit))
         }
-//        auth.signInAnonymously().addOnCompleteListener {
-//            if(it.isSuccessful) {
-//                Logger.d("Sign in SUCCESS: ${auth.currentUser}")
-//                result.value = Result.success(Unit)
-//            } else {
-//                Logger.d("Sign in FAILED: ${it.exception}")
-//                result.value = Result.failure(throw it.exception ?: Exception("Firebase Auth unknown exception"))
-//            }
-//        }
-        auth.signInAnonymously().addOnCompleteListener(AuthComplete(result))
-        return result
+        auth.signInAnonymously().addOnCompleteListener(authComplete)
+        return authComplete.authResult()
     }
 }
 
-class AuthComplete(private val result: MutableLiveData<Result<Unit>>) : OnCompleteListener<AuthResult> {
+class AuthComplete @Inject constructor() : OnCompleteListener<AuthResult> {
+    private val result = MutableLiveData<Result<Unit>>()
+
     override fun onComplete(p0: Task<AuthResult>) {
         if(p0.isSuccessful) {
             Logger.d("Sign in SUCCESS: ${p0.result.user}")
@@ -44,6 +39,8 @@ class AuthComplete(private val result: MutableLiveData<Result<Unit>>) : OnComple
             result.value = Result.failure(throw p0.exception ?: UNKNOWN_EXCEPTION)
         }
     }
+
+    fun authResult(): LiveData<Result<Unit>> = result
 
     companion object {
         val UNKNOWN_EXCEPTION = Exception("Firebase Auth unknown exception")
