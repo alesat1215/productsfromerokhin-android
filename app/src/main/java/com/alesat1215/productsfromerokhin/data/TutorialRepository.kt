a9a0467b26f1f6c968fa371fc38b032b2b07da54
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-07-29 11:13:50 +0300

    migrate to livedata

--- app/src/main/java/com/alesat1215/productsfromerokhin/data/TutorialRepository.kt
+++ app/src/main/java/com/alesat1215/productsfromerokhin/data/TutorialRepository.kt
@@ -1,10 +1,10 @@
 package com.alesat1215.productsfromerokhin.data
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
 import com.alesat1215.productsfromerokhin.data.local.ProductsDatabase
 import com.alesat1215.productsfromerokhin.util.RateLimiter
 import com.alesat1215.productsfromerokhin.util.RemoteConfigRepository
-import com.google.firebase.remoteconfig.FirebaseRemoteConfig
 import com.google.gson.Gson
 import com.orhanobut.logger.Logger
 import kotlinx.coroutines.Dispatchers
@@ -13,18 +13,18 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 import javax.inject.Singleton
 
-interface ITutorialRepository : RemoteConfigRepository {
+interface ITutorialRepository {
     /** @return instructions for order */
     fun instructions(): LiveData<List<Instruction>>
 }
 @Singleton
 class TutorialRepository @Inject constructor(
     /** Firebase remote config */
-    override val remoteConfig: FirebaseRemoteConfig,
+    private val remoteConfigRepository: RemoteConfigRepository,
     /** Room database */
     private val db: ProductsDatabase,
     /** Limiting the frequency of update database */
-    override val limiter: RateLimiter,
+    private val limiter: RateLimiter,
     /** For parse JSON from remote config */
     private val gson: Gson
 ) : ITutorialRepository {
@@ -40,15 +40,32 @@ class TutorialRepository @Inject constructor(
         // Return if limit is over
         if (!limiter.shouldFetch()) return
         // Fetch data from remote config
-        fetchAndActivate {
-            // Update data in Room
-            GlobalScope.launch(Dispatchers.IO) {
-                // Get instructions from JSON
-                val remoteInstructions = gson.fromJson(remoteConfig.getString(RemoteConfigRepository.INSTRUCTIONS), Array<Instruction>::class.java).asList()
-                Logger.d("Fetch instructions from remote config: ${remoteInstructions.count()}")
-                // Update Room
-                db.instructionsDao().updateInstructions(remoteInstructions)
-            }
+//        fetchAndActivate {
+//            // Update data in Room
+//            GlobalScope.launch(Dispatchers.IO) {
+//                // Get instructions from JSON
+//                val remoteInstructions = gson.fromJson(remoteConfig.getString(RemoteConfigRepository.INSTRUCTIONS), Array<Instruction>::class.java).asList()
+//                Logger.d("Fetch instructions from remote config: ${remoteInstructions.count()}")
+//                // Update Room
+//                db.instructionsDao().updateInstructions(remoteInstructions)
+//            }
+//        }
+        val observer = Observer<Result<Boolean>> {
+            it.onSuccess { updateInstructions() }
+            it.onFailure { Logger.d("Instructions is not update") }
+        }
+        remoteConfigRepository.fetchAndActivate().removeObserver(observer)
+        remoteConfigRepository.fetchAndActivate().observeForever(observer)
+    }
+
+    private fun updateInstructions() {
+        // Update data in Room
+        GlobalScope.launch(Dispatchers.IO) {
+            // Get instructions from JSON
+            val remoteInstructions = gson.fromJson(remoteConfigRepository.remoteConfig.getString(RemoteConfigRepository.INSTRUCTIONS), Array<Instruction>::class.java).asList()
+            Logger.d("Fetch instructions from remote config: ${remoteInstructions.count()}")
+            // Update Room
+            db.instructionsDao().updateInstructions(remoteInstructions)
         }
     }
 }
