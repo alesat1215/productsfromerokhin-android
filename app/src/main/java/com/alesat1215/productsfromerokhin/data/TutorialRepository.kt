70918e06121176a8e2883b1d395ecbb9bdc2e4e4
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-07-29 17:29:55 +0300

    migrate to FirebaseOnComplete

--- app/src/main/java/com/alesat1215/productsfromerokhin/data/TutorialRepository.kt
+++ app/src/main/java/com/alesat1215/productsfromerokhin/data/TutorialRepository.kt
@@ -1,10 +1,12 @@
 package com.alesat1215.productsfromerokhin.data
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
+import androidx.lifecycle.Transformations
 import com.alesat1215.productsfromerokhin.data.local.ProductsDatabase
 import com.alesat1215.productsfromerokhin.util.RateLimiter
-import com.alesat1215.productsfromerokhin.util.RemoteConfigRepository
+import com.alesat1215.productsfromerokhin.util.RemoteConfig
 import com.google.gson.Gson
 import com.orhanobut.logger.Logger
 import kotlinx.coroutines.Dispatchers
@@ -20,7 +22,7 @@ interface ITutorialRepository {
 @Singleton
 class TutorialRepository @Inject constructor(
     /** Firebase remote config */
-    private val remoteConfigRepository: RemoteConfigRepository,
+    private val remoteConfig: RemoteConfig,
     /** Room database */
     private val db: ProductsDatabase,
     /** Limiting the frequency of update database */
@@ -32,13 +34,14 @@ class TutorialRepository @Inject constructor(
     private val instructions by lazy { db.instructionsDao().instructions() }
 
     override fun instructions(): LiveData<List<Instruction>> {
-        updateDB()
-        return instructions
+//        updateDB()
+//        return instructions
+        return Transformations.switchMap(updateDB()) { instructions }
     }
     /** Update Room from remote config if needed */
-    private fun updateDB() {
+    private fun updateDB(): LiveData<Result<Unit>> {
         // Return if limit is over
-        if (!limiter.shouldFetch()) return
+        if (!limiter.shouldFetch()) return MutableLiveData(Result.success(Unit))
         // Fetch data from remote config
 //        fetchAndActivate {
 //            // Update data in Room
@@ -50,19 +53,24 @@ class TutorialRepository @Inject constructor(
 //                db.instructionsDao().updateInstructions(remoteInstructions)
 //            }
 //        }
-        val observer = Observer<Result<Boolean>> {
+//        val observer = Observer<Result<Boolean>> {
+//            it.onSuccess { updateInstructions() }
+//            it.onFailure { Logger.d("Instructions is not update") }
+//        }
+//        remoteConfig.fetchAndActivate().removeObserver(observer)
+//        remoteConfig.fetchAndActivate().observeForever(observer)
+        return Transformations.map(remoteConfig.fetchAndActivate()) {
             it.onSuccess { updateInstructions() }
-            it.onFailure { Logger.d("Instructions is not update") }
+            it.onFailure { Logger.d("Fetch remote config FAILED: ${it.localizedMessage}") }
+            it
         }
-        remoteConfigRepository.fetchAndActivate().removeObserver(observer)
-        remoteConfigRepository.fetchAndActivate().observeForever(observer)
     }
 
     private fun updateInstructions() {
         // Update data in Room
         GlobalScope.launch(Dispatchers.IO) {
             // Get instructions from JSON
-            val remoteInstructions = gson.fromJson(remoteConfigRepository.remoteConfig.getString(RemoteConfigRepository.INSTRUCTIONS), Array<Instruction>::class.java).asList()
+            val remoteInstructions = gson.fromJson(remoteConfig.firebaseRemoteConfig.getString(RemoteConfig.INSTRUCTIONS), Array<Instruction>::class.java).asList()
             Logger.d("Fetch instructions from remote config: ${remoteInstructions.count()}")
             // Update Room
             db.instructionsDao().updateInstructions(remoteInstructions)
--- app/src/main/java/com/alesat1215/productsfromerokhin/util/Auth.kt
+++ app/src/main/java/com/alesat1215/productsfromerokhin/util/Auth.kt
@@ -4,8 +4,6 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
-import com.google.android.gms.tasks.OnCompleteListener
-import com.google.android.gms.tasks.Task
 import com.orhanobut.logger.Logger
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -14,7 +12,7 @@ import javax.inject.Singleton
 @Singleton
 class Auth @Inject constructor(
     private val firebaseAuth: FirebaseAuth,
-    private val firebaseAuthComplete: FirebaseAuthComplete
+    private val firebaseOnComplete: FirebaseOnComplete<AuthResult>
 ) {
     /** @return result of sign in */
     fun signIn(): LiveData<Result<Unit>> {
@@ -24,33 +22,33 @@ class Auth @Inject constructor(
             return MutableLiveData(Result.success(Unit))
         }
         // Sign in as anonymous
-        firebaseAuth.signInAnonymously().addOnCompleteListener(firebaseAuthComplete)
-        return firebaseAuthComplete.authResult()
+        firebaseAuth.signInAnonymously().addOnCompleteListener(firebaseOnComplete)
+        return firebaseOnComplete.result()
     }
 }
-/** OnComplete callback for FirebaseAuth */
-class FirebaseAuthComplete @Inject constructor() : OnCompleteListener<AuthResult> {
-    /** Result of sign in */
-    private val result = MutableLiveData<Result<Unit>>()
-
-    override fun onComplete(p0: Task<AuthResult>) {
-        // Set success to result
-        if(p0.isSuccessful) {
-            Logger.d("Sign in SUCCESS: ${p0.result.user}")
-            result.value = Result.success(Unit)
-        // Set failure to result
-        } else {
-            val exception = p0.exception ?: Exception(UNKNOWN_EXCEPTION)
-            Logger.d("Sign in FAILED: $exception")
-            result.value = Result.failure(Throwable(exception))
-        }
-    }
-    /** @return LiveData with result of sign in */
-    fun authResult(): LiveData<Result<Unit>> = result
-
-    companion object {
-        /** Exception for sign in failure & exception is null */
-        const val UNKNOWN_EXCEPTION = "Firebase Auth unknown exception"
-    }
-
-}
+///** OnComplete callback for FirebaseAuth */
+//class FirebaseAuthComplete @Inject constructor() : OnCompleteListener<AuthResult> {
+//    /** Result of sign in */
+//    private val result = MutableLiveData<Result<Unit>>()
+//
+//    override fun onComplete(p0: Task<AuthResult>) {
+//        // Set success to result
+//        if(p0.isSuccessful) {
+//            Logger.d("Sign in SUCCESS: ${p0.result.user}")
+//            result.value = Result.success(Unit)
+//        // Set failure to result
+//        } else {
+//            val exception = p0.exception ?: Exception(UNKNOWN_EXCEPTION)
+//            Logger.d("Sign in FAILED: $exception")
+//            result.value = Result.failure(Throwable(exception))
+//        }
+//    }
+//    /** @return LiveData with result of sign in */
+//    fun authResult(): LiveData<Result<Unit>> = result
+//
+//    companion object {
+//        /** Exception for sign in failure & exception is null */
+//        const val UNKNOWN_EXCEPTION = "Firebase Auth unknown exception"
+//    }
+//
+//}
