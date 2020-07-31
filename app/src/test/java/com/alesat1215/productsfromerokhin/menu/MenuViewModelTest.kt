2c33e03968e8e35065e108b406ce7b5247f9a56d
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-07-31 14:48:28 +0300

    refactor MenuViewModelTest

--- app/src/test/java/com/alesat1215/productsfromerokhin/menu/MenuViewModelTest.kt
+++ app/src/test/java/com/alesat1215/productsfromerokhin/menu/MenuViewModelTest.kt
@@ -1,41 +1,44 @@
 package com.alesat1215.productsfromerokhin.menu
 
-import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-//import com.alesat1215.productsfromerokhin.RemoteDataMockTest
+import androidx.lifecycle.LiveData
+import com.alesat1215.productsfromerokhin.data.Group
+import com.alesat1215.productsfromerokhin.data.ProductInfo
 import com.alesat1215.productsfromerokhin.data.ProductsRepository
+import org.junit.Before
 
+import org.junit.Assert.*
 import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
+import org.mockito.Mockito.`when`
 import org.mockito.junit.MockitoJUnitRunner
 
 @RunWith(MockitoJUnitRunner::class)
 class MenuViewModelTest {
     @Mock
     private lateinit var repository: ProductsRepository
+    @Mock
+    private lateinit var products: LiveData<List<ProductInfo>>
+    @Mock
+    private lateinit var groups: LiveData<List<Group>>
+
     private lateinit var viewModel: MenuViewModel
 
-    @get:Rule
-    val instantTaskExecutorRule = InstantTaskExecutorRule()
+    @Before
+    fun setUp() {
+        `when`(repository.groups()).thenReturn(groups)
+        `when`(repository.products()).thenReturn(products)
+        viewModel = MenuViewModel(repository)
+    }
 
-//    @Before
-//    fun setUp() {
-//        `when`(repository.groups()).thenReturn(MutableLiveData(RemoteDataMockTest.data.groups()))
-//        `when`(repository.products()).thenReturn(MutableLiveData(RemoteDataMockTest.productsNotEmptyCart))
-//        viewModel = MenuViewModel(repository)
-//    }
-//
-//    @Test
-//    fun products() {
-//        var products = listOf<Product>()
-//        viewModel.products().observeForever { products = it }
-//        assertEquals(products, RemoteDataMockTest.productsNotEmptyCart)
-//    }
-//
-//    @Test
-//    fun groups() {
-//        var groups = listOf<GroupDB>()
-//        viewModel.groups().observeForever { groups = it }
-//        assertEquals(groups, RemoteDataMockTest.data.groups())
-//    }
+    @Test
+    fun products() {
+        assertEquals(viewModel.products(), products)
+    }
+
+    @Test
+    fun groups() {
+        assertEquals(viewModel.groups(), groups)
+    }
 }
