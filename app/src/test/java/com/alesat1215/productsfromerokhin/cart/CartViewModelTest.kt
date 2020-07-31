e68d117370b85a9d1f8170087b28fceee370cb9b
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-07-31 13:23:41 +0300

    refactor CartViewModelTest

--- app/src/test/java/com/alesat1215/productsfromerokhin/cart/CartViewModelTest.kt
+++ app/src/test/java/com/alesat1215/productsfromerokhin/cart/CartViewModelTest.kt
@@ -1,39 +1,61 @@
 package com.alesat1215.productsfromerokhin.cart
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.alesat1215.productsfromerokhin.data.ProductInfo
 //import com.alesat1215.productsfromerokhin.RemoteDataMockTest
 import com.alesat1215.productsfromerokhin.data.ProductsRepository
+import com.alesat1215.productsfromerokhin.data.ProfileRepository
 import com.alesat1215.productsfromerokhin.profileMockTest
 import kotlinx.coroutines.newSingleThreadContext
 import kotlinx.coroutines.runBlocking
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Before
 import org.junit.Rule
 import org.junit.runner.RunWith
 import org.mockito.Mock
+import org.mockito.Mockito.`when`
 import org.mockito.Mockito.verify
 import org.mockito.junit.MockitoJUnitRunner
 
 @RunWith(MockitoJUnitRunner::class)
 class CartViewModelTest {
     @Mock
-    private lateinit var repository: ProductsRepository
+    private lateinit var productsRepository: ProductsRepository
+    @Mock
+    private lateinit var profileRepository: ProfileRepository
+    @Mock
+    private lateinit var productInfo: ProductInfo
+    private lateinit var products: LiveData<List<ProductInfo>>
+
     private lateinit var viewModel: CartViewModel
-    private val mainThreadSurrogate = newSingleThreadContext("UI thread")
+
+//    private val mainThreadSurrogate = newSingleThreadContext("UI thread")
 
     @get:Rule
     val instantTaskExecutorRule = InstantTaskExecutorRule()
 
-//    @Before
-//    fun setUp() {
+    @Before
+    fun setUp() {
 //        Dispatchers.setMain(mainThreadSurrogate)
 //        `when`(repository.productsInCart)
 //            .thenReturn(MutableLiveData(RemoteDataMockTest.productsNotEmptyCart))
 //        `when`(repository.profile).thenReturn(MutableLiveData(profileMockTest()))
 //        viewModel = CartViewModel(repository)
-//    }
-//
+        products = MutableLiveData(listOf(productInfo))
+        `when`(productsRepository.productsInCart).thenReturn(products)
+        viewModel = CartViewModel(productsRepository, profileRepository)
+    }
+
+    @Test
+    fun products() {
+        assertEquals(viewModel.products(), products)
+    }
+
+    //
 //    @After
 //    fun tearDown() {
 //        Dispatchers.resetMain() // reset main dispatcher to the original Main dispatcher
@@ -75,6 +97,6 @@ class CartViewModelTest {
     @Test
     fun clearCart() = runBlocking {
         viewModel.clearCart()
-        verify(repository).clearCart()
+        verify(productsRepository).clearCart()
     }
 }
