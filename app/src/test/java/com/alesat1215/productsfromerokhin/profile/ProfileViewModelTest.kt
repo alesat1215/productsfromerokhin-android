86e3b5e93211aaf6cdb012e76810fbb4533de5f1
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-07-31 15:15:26 +0300

    refactor ProfileViewModelTest

--- app/src/test/java/com/alesat1215/productsfromerokhin/profile/ProfileViewModelTest.kt
+++ app/src/test/java/com/alesat1215/productsfromerokhin/profile/ProfileViewModelTest.kt
@@ -1,13 +1,12 @@
 package com.alesat1215.productsfromerokhin.profile
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.alesat1215.productsfromerokhin.data.Profile
 import com.alesat1215.productsfromerokhin.data.ProfileRepository
 import com.alesat1215.productsfromerokhin.profileMockTest
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.newSingleThreadContext
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.setMain
 import org.junit.After
@@ -22,20 +21,25 @@ import org.mockito.Mockito.`when`
 import org.mockito.Mockito.verify
 import org.mockito.junit.MockitoJUnitRunner
 
+@ObsoleteCoroutinesApi
+@ExperimentalCoroutinesApi
 @RunWith(MockitoJUnitRunner::class)
 class ProfileViewModelTest {
     @Mock
     private lateinit var repository: ProfileRepository
     private lateinit var viewModel: ProfileViewModel
+    @Mock
+    private lateinit var profile: LiveData<Profile>
+
     private val mainThreadSurrogate = newSingleThreadContext("UI thread")
 
-    @get:Rule
-    val instantTaskExecutorRule = InstantTaskExecutorRule()
+//    @get:Rule
+//    val instantTaskExecutorRule = InstantTaskExecutorRule()
 
     @Before
     fun setUp() {
         Dispatchers.setMain(mainThreadSurrogate)
-        `when`(repository.profile).thenReturn(MutableLiveData(profileMockTest()))
+        `when`(repository.profile).thenReturn(profile)
         viewModel = ProfileViewModel(repository)
     }
 
@@ -47,9 +51,7 @@ class ProfileViewModelTest {
 
     @Test
     fun profile() {
-        var profile: Profile? = null
-        viewModel.profile.observeForever { profile = it }
-        assertEquals(profile, profileMockTest())
+        assertEquals(viewModel.profile, profile)
     }
 
     @Test
