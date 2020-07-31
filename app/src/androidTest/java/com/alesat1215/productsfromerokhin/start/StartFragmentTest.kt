b31aeb6c1a14831303c97f57026afb7d888a1129
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-07-31 18:42:21 +0300

    refactor StartFragmentTest

--- app/src/androidTest/java/com/alesat1215/productsfromerokhin/start/StartFragmentTest.kt
+++ app/src/androidTest/java/com/alesat1215/productsfromerokhin/start/StartFragmentTest.kt
@@ -6,6 +6,7 @@ import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
+import com.alesat1215.productsfromerokhin.DataMock
 import com.alesat1215.productsfromerokhin.R
 import org.junit.Before
 
@@ -25,30 +26,16 @@ class StartFragmentTest {
 
     @Test
     fun checkViews() {
-        sleep(10000)
-        onView(withId(R.id.title_start)).check(matches(withText("")))
+        // Titles
+        onView(withId(R.id.title_start)).check(matches(withText(DataMock.titles.title)))
+        onView(withId(R.id.imgTitle_start)).check(matches(withText(DataMock.titles.imgTitle)))
+        onView(withId(R.id.productsTitle_start)).check(matches(withText(DataMock.titles.productsTitle)))
+        onView(withId(R.id.productsTitle2_start)).check(matches(withText(DataMock.titles.productsTitle2)))
+        // Products
+        DataMock.products.filter { it.product?.inStart == true && it.product?.inStart2 == true }.forEach {
+            onView(withText(it.product?.name)).check(matches(isDisplayed()))
+            onView(withText(it.inCart.toString())).check(matches(isDisplayed()))
+        }
     }
 
-//    @Test
-//    fun checkViews() {
-//        // Titles
-//        onView(withId(R.id.title_start)).check(matches(withText(RemoteDataMockAndroidTest.data.title)))
-//        onView(withId(R.id.imgTitle_start)).check(matches(withText(RemoteDataMockAndroidTest.data.imgTitle)))
-//        onView(withId(R.id.productsTitle_start)).check(matches(withText(RemoteDataMockAndroidTest.data.productsTitle)))
-//        onView(withId(R.id.productsTitle2_start)).check(matches(withText(RemoteDataMockAndroidTest.data.productsTitle2)))
-//        // Products
-//        val products = RemoteDataMockAndroidTest.products123AndroidTest()
-//        onView(withText(products.first().name)).check(matches(isDisplayed()))
-//        onView(withId(R.id.products_start)).perform(scrollToPosition<BindViewHolder>(1))
-//        onView(withText(products[1].name)).check(matches(isDisplayed()))
-//        onView(withId(R.id.products_start)).perform(scrollToPosition<BindViewHolder>(products.count() - 1))
-//        onView(withText(products.last().name)).check(matches(isDisplayed()))
-//        // Products2
-//        val products2 = RemoteDataMockAndroidTest.products456AndroidTest()
-//        onView(withText(products2.first().name)).check(matches(isDisplayed()))
-//        onView(withId(R.id.products2_start)).perform(scrollToPosition<BindViewHolder>(1))
-//        onView(withText(products2[1].name)).check(matches(isDisplayed()))
-//        onView(withId(R.id.products2_start)).perform(scrollToPosition<BindViewHolder>(products2.count() - 1))
-//        onView(withText(products2.last().name)).check(matches(isDisplayed()))
-//    }
 }
