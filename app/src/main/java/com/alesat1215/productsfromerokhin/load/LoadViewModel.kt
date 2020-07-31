320513281c1a9c32f4a87ca20b6e435a9b5b21b1
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-07-31 20:30:21 +0300

    add phone to viewmodel & fragment

--- app/src/main/java/com/alesat1215/productsfromerokhin/load/LoadViewModel.kt
+++ app/src/main/java/com/alesat1215/productsfromerokhin/load/LoadViewModel.kt
@@ -2,14 +2,17 @@ package com.alesat1215.productsfromerokhin.load
 
 import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
+import com.alesat1215.productsfromerokhin.data.IPhoneRepository
 import com.alesat1215.productsfromerokhin.data.IProductsRepository
 import com.alesat1215.productsfromerokhin.data.ITutorialRepository
+import com.alesat1215.productsfromerokhin.data.PhoneRepository
 import com.alesat1215.productsfromerokhin.util.Auth
 import javax.inject.Inject
 
 class LoadViewModel @Inject constructor(
     private val productsRepository: IProductsRepository,
     private val tutorialRepository: ITutorialRepository,
+    private val phoneRepository: IPhoneRepository,
     private val auth: Auth
 ) : ViewModel() {
 
@@ -17,4 +20,5 @@ class LoadViewModel @Inject constructor(
     /** Trigger of data loading */
     fun loadCompleteProducts() = Transformations.map(productsRepository.products()) { it.isNotEmpty() }
     fun loadCompleteTutorial() = Transformations.map(tutorialRepository.instructions()) { it.isNotEmpty() }
+    fun loadCompletePhone() = Transformations.map(phoneRepository.phone()) { it.phone.isNotEmpty() }
 }
