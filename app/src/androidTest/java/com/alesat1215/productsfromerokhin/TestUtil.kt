b6c552fbea86d5e44b0b9d39a23da8ca36172682
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-07-31 19:01:40 +0300

    refactor MainActivityTest

--- app/src/androidTest/java/com/alesat1215/productsfromerokhin/TestUtil.kt
+++ app/src/androidTest/java/com/alesat1215/productsfromerokhin/TestUtil.kt
@@ -3,34 +3,6 @@ package com.alesat1215.productsfromerokhin
 import com.alesat1215.productsfromerokhin.data.*
 
 object DataMock {
-//    val data = RemoteData(
-//    "Title Test",
-//    "Img Test",
-//    "ImgTitle Test",
-//    "ProductsTitle Test",
-//    "ProductsTitle2 Test",
-//    listOf(
-//        GroupRemote("Group 1 Test").apply { products = products123AndroidTest() },
-//        GroupRemote("Group 2 Test").apply { products = products456AndroidTest() }
-//    )
-//    )
-//
-//    fun products123AndroidTest() = listOf(
-//        ProductRemote(name = "Product 1 Test", inStart = true),
-//        ProductRemote(name = "Product 2 Test", inStart = true),
-//        ProductRemote(name = "Product 3 Test", inStart = true)
-//    )
-//    fun products456AndroidTest() = listOf(
-//        ProductRemote(name = "Product 4 Test", inStart2 = true),
-//        ProductRemote(name = "Product 5 Test", inStart2 = true),
-//        ProductRemote(name = "Product 6 Test", inStart2 = true)
-//    )
-//
-//    val productsEmptyCart by lazy { data.products().map {
-//        Product(it, emptyList())
-//    } }
-//
-//    val productsForCart by lazy { productsEmptyCart.map { it.asProductInCart() } }
     val titles = Titles("title", "img", "imgTitle", "productsTitle", "productsTitle2")
     val groups = listOf(
         Group("group 1").apply { products = listOf(Product(name = "product 1"), Product(name = "product 2", inStart = true)) },
@@ -38,10 +10,5 @@ object DataMock {
         Group("group 3").apply { products = listOf(Product(name = "product 5"), Product(name = "product 6")) }
     )
     val products = products(groups).map { ProductInfo(it, listOf(ProductInCart(name = it.name))) }
+    val profile = Profile(name = "name", phone = "phone", address = "address")
 }
-
-fun profileMockAndroidTest() = Profile(
-    name = "name",
-    phone = "phone",
-    address = "address"
-)
