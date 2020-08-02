6df12d9d852ea2bba4bca4ec0cf03b6e85116152
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-08-02 18:28:24 +0300

    migrate to DatabaseUpdater

--- app/src/main/java/com/alesat1215/productsfromerokhin/data/PhoneRepository.kt
+++ app/src/main/java/com/alesat1215/productsfromerokhin/data/PhoneRepository.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
 import androidx.lifecycle.liveData
+import com.alesat1215.productsfromerokhin.util.DatabaseUpdater
 import com.alesat1215.productsfromerokhin.util.UpdateLimiter
 import com.alesat1215.productsfromerokhin.util.RemoteConfig
 import com.orhanobut.logger.Logger
@@ -15,7 +16,7 @@ import javax.inject.Singleton
 /** Repository for phone number for order.
  * Return LiveData from Room & update if needed Room from remote config.
  * */
-interface IPhoneRepository {
+interface IPhoneRepository : DatabaseUpdater {
     /** Get phone & update Room from remote config if needed */
     fun phone(): LiveData<PhoneForOrder?>
 }
@@ -23,33 +24,33 @@ interface IPhoneRepository {
 @Singleton
 class PhoneRepository @Inject constructor(
     /** Firebase remote config */
-    private val remoteConfig: RemoteConfig,
+    override val remoteConfig: RemoteConfig,
     /** Room database */
     private val db: AppDatabase,
     /** Limiting the frequency of queries to remote config & update db */
-    private val limiter: UpdateLimiter
+    override val limiter: UpdateLimiter
 ) : IPhoneRepository {
     /** @return LiveData with phone from Room only once */
     private val phone by lazy { db.phoneDao().phone() }
 
     override fun phone(): LiveData<PhoneForOrder?> {
-        return Transformations.switchMap(updateDB()) { phone }
+        return Transformations.switchMap(updateDB(::updatePhone)) { phone }
     }
     /** Update Room from remote config if needed */
-    private fun updateDB(): LiveData<Result<Unit>> {
-        /** Return if limit is over */
-        if (limiter.needUpdate().not()) return MutableLiveData(Result.success(Unit))
-        // Fetch data from remote config & update db
-        return Transformations.switchMap(remoteConfig.fetchAndActivate()) {
-            liveData {
-                it.onSuccess { emit(Result.success(updatePhone())) }
-                it.onFailure {
-                    Logger.d("Fetch remote config FAILED: ${it.localizedMessage}")
-                    emit(Result.failure(it))
-                }
-            }
-        }
-    }
+//    private fun updateDB(): LiveData<Result<Unit>> {
+//        /** Return if limit is over */
+//        if (limiter.needUpdate().not()) return MutableLiveData(Result.success(Unit))
+//        // Fetch data from remote config & update db
+//        return Transformations.switchMap(remoteConfig.fetchAndActivate()) {
+//            liveData {
+//                it.onSuccess { emit(Result.success(updatePhone())) }
+//                it.onFailure {
+//                    Logger.d("Fetch remote config FAILED: ${it.localizedMessage}")
+//                    emit(Result.failure(it))
+//                }
+//            }
+//        }
+//    }
     /** Update data in Room in background */
     private suspend fun updatePhone() = withContext(Dispatchers.Default) {
         // Get phone from remote config
