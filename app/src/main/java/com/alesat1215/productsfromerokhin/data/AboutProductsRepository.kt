fecd23aef9820dbe7aeb39477a72a5d9b525eb50
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-08-04 18:08:22 +0300

    about products dao for title

--- app/src/main/java/com/alesat1215/productsfromerokhin/data/AboutProductsRepository.kt
+++ app/src/main/java/com/alesat1215/productsfromerokhin/data/AboutProductsRepository.kt
@@ -1,7 +1,10 @@
 package com.alesat1215.productsfromerokhin.data
 
+import androidx.lifecycle.LiveData
 import com.alesat1215.productsfromerokhin.util.DatabaseUpdater
 import com.google.gson.Gson
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -13,5 +16,22 @@ class AboutProductsRepository @Inject constructor(
     /** For parse JSON from remote config */
     private val gson: Gson
 ) {
+    private val aboutProducts by lazy { db.aboutProductsDao().aboutProducts() }
 
+    fun aboutProducts(): LiveData<List<AboutProducts>> {
+
+    }
+
+    private suspend fun updateAboutProducts() = withContext(Dispatchers.Default) {
+        val aboutProducts = gson.fromJson(
+            dbUpdater.firebaseRemoteConfig.getString(ABOUT_PRODUCTS_LIST),
+            Array<AboutProducts>::class.java
+        ).asList()
+    }
+
+    companion object {
+        /** Parameters in Firebase remote config */
+        const val ABOUT_PRODUCTS_LIST = "about_products_list"
+        const val ABOUT_PRODUCTS_TITLE = "about_products_list_title"
+    }
 }
