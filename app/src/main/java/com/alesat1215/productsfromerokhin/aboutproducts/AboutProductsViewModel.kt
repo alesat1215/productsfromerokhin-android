87cc223e0f4aa0d6fb69adbbda19974d40e7156b
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-08-04 20:45:31 +0300

    add AboutProductsViewModel

---
+++ app/src/main/java/com/alesat1215/productsfromerokhin/aboutproducts/AboutProductsViewModel.kt
@@ -0,0 +1,22 @@
+package com.alesat1215.productsfromerokhin.aboutproducts
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Transformations
+import androidx.lifecycle.ViewModel
+import com.alesat1215.productsfromerokhin.data.AboutProducts
+import com.alesat1215.productsfromerokhin.data.IAboutProductsRepository
+import javax.inject.Inject
+
+class AboutProductsViewModel @Inject constructor(
+    private val aboutProductsRepository: IAboutProductsRepository
+): ViewModel() {
+    /** @return about products title */
+    val title = aboutProductsRepository.aboutProductsTitle()
+
+    /** @return about products filtering by predicate */
+    fun aboutProducts(predicate: ((AboutProducts) -> Boolean)? = null): LiveData<List<AboutProducts>> {
+        return if (predicate != null)
+            Transformations.map(aboutProductsRepository.aboutProducts()) { it.filter(predicate) }
+        else aboutProductsRepository.aboutProducts()
+    }
+}
