58d59a0fdea9d6b24e149c57bf3e152945a9e027
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-08-06 21:21:11 +0300

    refactor ContactsFragmentTest

--- app/src/androidTest/java/com/alesat1215/productsfromerokhin/contacts/ContactsFragmentTest.kt
+++ app/src/androidTest/java/com/alesat1215/productsfromerokhin/contacts/ContactsFragmentTest.kt
@@ -1,18 +1,28 @@
 package com.alesat1215.productsfromerokhin.contacts
 
+import android.content.Intent
+import android.net.Uri
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.rule.IntentsTestRule
 import androidx.test.espresso.matcher.ViewMatchers.*
 import com.alesat1215.productsfromerokhin.DataMock
+import com.alesat1215.productsfromerokhin.MainActivity
 import com.alesat1215.productsfromerokhin.R
 import org.junit.After
 import org.junit.Before
 
 import org.junit.Assert.*
+import org.junit.Rule
 import org.junit.Test
 
 class ContactsFragmentTest {
+
+    @get:Rule
+    val intentsTestRule = IntentsTestRule(MainActivity::class.java)
 
     @Before
     fun setUp() {
@@ -30,5 +40,16 @@ class ContactsFragmentTest {
         onView(withText(R.string.our_address)).check(matches(isDisplayed()))
         onView(withText(DataMock.contacts.address)).check(matches(isDisplayed()))
         onView(withId(R.id.contacts_call)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun call() {
+        onView(withId(R.id.contacts_call)).perform(click())
+        val chooser = Intents.getIntents().last()
+        assertEquals(chooser.action, Intent.ACTION_CHOOSER)
+        assertTrue(chooser.hasExtra(Intent.EXTRA_INTENT))
+        val call = chooser.getParcelableExtra<Intent>(Intent.EXTRA_INTENT)
+        assertEquals(call?.action, Intent.ACTION_DIAL)
+        assertEquals(call?.data, Uri.parse("tel:${DataMock.contacts.phone}"))
     }
 }
