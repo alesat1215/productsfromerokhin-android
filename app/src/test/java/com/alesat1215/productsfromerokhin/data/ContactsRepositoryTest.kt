d122ac7e00b33db84c012fe379f5e27c60c70bff
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-08-06 16:57:25 +0300

    refactor ContactsRepositoryTest

--- app/src/test/java/com/alesat1215/productsfromerokhin/data/ContactsRepositoryTest.kt
+++ app/src/test/java/com/alesat1215/productsfromerokhin/data/ContactsRepositoryTest.kt
@@ -6,6 +6,7 @@ import com.alesat1215.productsfromerokhin.util.DatabaseUpdater
 import com.alesat1215.productsfromerokhin.util.RemoteConfig
 import com.alesat1215.productsfromerokhin.util.UpdateLimiter
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import com.google.gson.Gson
 import kotlinx.coroutines.*
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.setMain
@@ -37,8 +38,9 @@ class ContactsRepositoryTest {
     private lateinit var dbUpdater: DatabaseUpdater
     @Mock
     private lateinit var contactsDao: ContactsDao
-
-    private val phoneForOrder = Contacts("phone")
+    @Mock
+    private lateinit var gson: Gson
+    private val contacts = Contacts()
 
     private lateinit var repository: ContactsRepository
 
@@ -51,11 +53,12 @@ class ContactsRepositoryTest {
     fun setUp() {
         Dispatchers.setMain(mainThreadSurrogate)
         `when`(db.contactsDao()).thenReturn(contactsDao)
-        `when`(db.contactsDao().contacts()).thenReturn(MutableLiveData(phoneForOrder))
-        `when`(firebaseRemoteConfig.getString(ContactsRepository.CONTACTS)).thenReturn(phoneForOrder.phone)
+        `when`(db.contactsDao().contacts()).thenReturn(MutableLiveData(contacts))
+        `when`(firebaseRemoteConfig.getString(ContactsRepository.CONTACTS)).thenReturn("")
+        `when`(gson.fromJson("", Contacts::class.java)).thenReturn(contacts)
         `when`(remoteConfig.firebaseRemoteConfig).thenReturn(firebaseRemoteConfig)
         dbUpdater = DatabaseUpdater(limiter, remoteConfig)
-        repository = ContactsRepository(db, dbUpdater)
+        repository = ContactsRepository(db, dbUpdater, gson)
     }
 
     @After
@@ -65,21 +68,21 @@ class ContactsRepositoryTest {
     }
 
     @Test
-    fun phone() = runBlocking {
+    fun contacts() = runBlocking {
         // Not update db
         var result: Contacts? = null
         `when`(limiter.needUpdate()).thenReturn(false)
         repository.contacts().observeForever { result = it }
         sleep(100)
-        assertEquals(result, phoneForOrder)
-        verify(contactsDao, never()).updateContacts(phoneForOrder)
+        assertEquals(result, contacts)
+        verify(contactsDao, never()).updateContacts(contacts)
         // Update db
         result = null
         `when`(limiter.needUpdate()).thenReturn(true)
         `when`(remoteConfig.fetchAndActivate()).thenReturn(MutableLiveData(Result.success(Unit)))
         repository.contacts().observeForever { result = it }
         sleep(100)
-        assertEquals(result, phoneForOrder)
-        verify(contactsDao).updateContacts(phoneForOrder)
+        assertEquals(result, contacts)
+        verify(contactsDao).updateContacts(contacts)
     }
 }
