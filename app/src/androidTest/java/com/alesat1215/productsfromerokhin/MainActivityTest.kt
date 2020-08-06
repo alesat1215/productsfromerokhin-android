f9ef6c29e92b0483745d0c8530c4cc6b78475bf5
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-08-06 12:18:02 +0300

    refactor MainActivityTest

--- app/src/androidTest/java/com/alesat1215/productsfromerokhin/MainActivityTest.kt
+++ app/src/androidTest/java/com/alesat1215/productsfromerokhin/MainActivityTest.kt
@@ -1,6 +1,7 @@
 package com.alesat1215.productsfromerokhin
 
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.Espresso.pressBack
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
@@ -11,6 +12,7 @@ import org.hamcrest.core.IsNot.not
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.lang.Thread.sleep
 
 @RunWith(AndroidJUnit4::class)
 @LargeTest
@@ -47,5 +49,11 @@ class MainActivityTest {
         onView(withText(R.string.tutorial)).perform(click())
         onView(withText(DataMock.instructions.first().text)).check(matches(isDisplayed()))
         onView(withId(R.id.nav_view)).check(matches(not((isDisplayed()))))
+        pressBack()
+        onView(withId(R.id.nav_view)).check(matches((isDisplayed())))
+        // Navigate to about products
+        onView(withText(R.string.about_products)).perform(click())
+        onView(withId(R.id.about_products_list)).check(matches(isDisplayed()))
+        onView(withId(R.id.about_products_list2)).check(matches(isDisplayed()))
     }
 }
