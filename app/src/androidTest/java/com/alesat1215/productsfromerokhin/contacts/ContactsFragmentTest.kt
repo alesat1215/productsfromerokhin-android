1d453c18b17e9867a6d1449573e14705f9354f82
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-08-06 20:26:49 +0300

    add ContactsFragmentTest

--- app/src/androidTest/java/com/alesat1215/productsfromerokhin/contacts/ContactsFragmentTest.kt
+++ app/src/androidTest/java/com/alesat1215/productsfromerokhin/contacts/ContactsFragmentTest.kt
@@ -1,17 +1,34 @@
 package com.alesat1215.productsfromerokhin.contacts
 
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import com.alesat1215.productsfromerokhin.DataMock
+import com.alesat1215.productsfromerokhin.R
 import org.junit.After
 import org.junit.Before
 
 import org.junit.Assert.*
+import org.junit.Test
 
 class ContactsFragmentTest {
 
     @Before
     fun setUp() {
+        launchFragmentInContainer<ContactsFragment>(themeResId = R.style.AppTheme)
     }
 
-    @After
-    fun tearDown() {
+    @Test
+    fun checkViews() {
+        onView(withId(R.id.contacts_phone_title)).check(matches(isDisplayed()))
+        onView(withText(R.string.our_phone)).check(matches(isDisplayed()))
+        onView(withId(R.id.contacts_phone_text)).check(matches(isDisplayed()))
+        onView(withText(DataMock.contacts.phone)).check(matches(isDisplayed()))
+        onView(withId(R.id.contacts_address_title)).check(matches(isDisplayed()))
+        onView(withId(R.id.contacts_address_text)).check(matches(isDisplayed()))
+        onView(withText(R.string.our_address)).check(matches(isDisplayed()))
+        onView(withText(DataMock.contacts.address)).check(matches(isDisplayed()))
+        onView(withId(R.id.contacts_call)).check(matches(isDisplayed()))
     }
 }
