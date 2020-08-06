3e052a56b8baef4fbe0cd510b16947ed3fdab70c
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-08-06 12:39:41 +0300

    add TutorialFragmentTest

--- app/src/androidTest/java/com/alesat1215/productsfromerokhin/tutorial/TutorialFragmentTest.kt
+++ app/src/androidTest/java/com/alesat1215/productsfromerokhin/tutorial/TutorialFragmentTest.kt
@@ -1,14 +1,40 @@
 package com.alesat1215.productsfromerokhin.tutorial
 
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.swipeLeft
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import com.alesat1215.productsfromerokhin.DataMock
+import com.alesat1215.productsfromerokhin.R
+import org.hamcrest.Matchers.allOf
 import org.junit.After
 import org.junit.Before
 
 import org.junit.Assert.*
+import org.junit.Test
+import org.junit.runner.RunWith
 
+@LargeTest
+@RunWith(AndroidJUnit4::class)
 class TutorialFragmentTest {
 
     @Before
     fun setUp() {
+        launchFragmentInContainer<TutorialFragment>(themeResId = R.style.AppTheme)
+    }
+
+    @Test
+    fun checkViews() {
+        DataMock.instructions.forEach {
+            onView(withText(it.title)).check(matches(isDisplayed()))
+            onView(withText(it.text)).check(matches(isDisplayed()))
+            onView(withId(R.id.pager_tutorial)).perform(swipeLeft())
+        }
     }
 
     @After
