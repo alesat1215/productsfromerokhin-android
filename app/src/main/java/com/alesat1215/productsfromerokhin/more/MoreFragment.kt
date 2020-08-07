0bea335304c111130e28aa3ad18478b817b74f61
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-08-07 18:07:38 +0300

    add share

--- app/src/main/java/com/alesat1215/productsfromerokhin/more/MoreFragment.kt
+++ app/src/main/java/com/alesat1215/productsfromerokhin/more/MoreFragment.kt
@@ -1,12 +1,12 @@
 package com.alesat1215.productsfromerokhin.more
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import com.alesat1215.productsfromerokhin.databinding.FragmentMoreBinding
-import kotlinx.android.synthetic.main.activity_main.*
+import com.orhanobut.logger.Logger
 
 /**
  * [MoreFragment] subclass of [Fragment].
@@ -17,6 +17,24 @@ class MoreFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ) = FragmentMoreBinding.inflate(inflater, container, false).root
+    ) = FragmentMoreBinding.inflate(inflater, container, false).apply {
+        fragment = this@MoreFragment
+    }.root
 
+    fun share() {
+        // Create intent
+        val intent = Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_TEXT, "https://play.google.com/store/apps/details?id=${activity?.packageName}")
+            type = "text/plain"
+        }
+        // Show chooser
+        val chooser: Intent = Intent.createChooser(intent, "")
+        activity?.packageManager?.also {
+            intent.resolveActivity(it)?.also {
+                startActivity(chooser)
+                Logger.d("Select messenger for share")
+            }
+        }
+    }
 }
