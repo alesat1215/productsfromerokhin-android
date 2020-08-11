a3ae37ca9d38c97bc1970c2fea35329f9edf5ad4
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-08-11 12:08:57 +0300

    add loadDataComplete to viewModel

--- app/src/main/java/com/alesat1215/productsfromerokhin/load/LoadViewModel.kt
+++ app/src/main/java/com/alesat1215/productsfromerokhin/load/LoadViewModel.kt
@@ -1,9 +1,12 @@
 package com.alesat1215.productsfromerokhin.load
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 import com.alesat1215.productsfromerokhin.data.*
 import com.alesat1215.productsfromerokhin.util.Auth
+import com.orhanobut.logger.Logger
 import javax.inject.Inject
 
 class LoadViewModel @Inject constructor(
@@ -12,6 +15,7 @@ class LoadViewModel @Inject constructor(
     private val contactsRepository: IContactsRepository,
     private val titlesRepository: ITitlesRepository,
     private val aboutProductsRepository: IAboutProductsRepository,
+    private val aboutAppRepository: IAboutAppRepository,
     private val auth: Auth
 ) : ViewModel() {
 
@@ -22,4 +26,47 @@ class LoadViewModel @Inject constructor(
     fun loadCompletePhone() = Transformations.map(contactsRepository.contacts()) { it != null }
     fun loadCompleteTitles() = Transformations.map(titlesRepository.titles()) { it != null }
     fun loadCompleteAboutProducts() = Transformations.map(aboutProductsRepository.aboutProducts()) { it.isNotEmpty() }
+    fun loadCompleteAboutApp() = Transformations.map(aboutAppRepository.aboutApp()) { it != null }
+
+    fun loadDataComplete(): LiveData<Boolean> {
+        val phone by lazy { Transformations.map(contactsRepository.contacts()) { it != null } }
+        val titles by lazy { Transformations.map(titlesRepository.titles()) { it != null } }
+        val products by lazy { Transformations.map(productsRepository.products()) { it.isNotEmpty() } }
+        val aboutProducts by lazy { Transformations.map(aboutProductsRepository.aboutProducts()) { it.isNotEmpty() } }
+        val aboutApp by lazy { Transformations.map(aboutAppRepository.aboutApp()) { it != null } }
+
+        val phone_titles = Transformations.switchMap(phone) {
+            if (it) {
+                Logger.d("Load phone complete")
+                return@switchMap titles
+            } else {
+                return@switchMap MutableLiveData(it)
+            }
+        }
+        val phone_titles_products = Transformations.switchMap(phone_titles) {
+            if (it) {
+                Logger.d("Load titles complete")
+                return@switchMap products
+            } else {
+                return@switchMap MutableLiveData(it)
+            }
+        }
+        val phone_titles_products_aboutProducts = Transformations.switchMap(phone_titles_products) {
+            if (it) {
+                Logger.d("Load products complete")
+                return@switchMap aboutProducts
+            } else {
+                return@switchMap MutableLiveData(it)
+            }
+        }
+
+        return Transformations.switchMap(phone_titles_products_aboutProducts) {
+            if (it) {
+                Logger.d("Load about products complete")
+                return@switchMap aboutApp
+            } else {
+                return@switchMap MutableLiveData(it)
+            }
+        }
+    }
 }
