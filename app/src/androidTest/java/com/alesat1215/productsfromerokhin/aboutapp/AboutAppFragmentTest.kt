fe3882871122b7f083b53738a275c8f667c1da99
Alexander Satunin <aleksandr.satunin@sparm.com>
2020-08-11 13:55:45 +0300

    refactor AboutAppFragmentTest

--- app/src/androidTest/java/com/alesat1215/productsfromerokhin/aboutapp/AboutAppFragmentTest.kt
+++ app/src/androidTest/java/com/alesat1215/productsfromerokhin/aboutapp/AboutAppFragmentTest.kt
@@ -1,14 +1,22 @@
 package com.alesat1215.productsfromerokhin.aboutapp
 
+import android.content.Intent
+import android.net.Uri
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.rule.IntentsTestRule
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
+import com.alesat1215.productsfromerokhin.DataMock
+import com.alesat1215.productsfromerokhin.MainActivity
 import com.alesat1215.productsfromerokhin.R
 import com.alesat1215.productsfromerokhin.aboutproducts.AboutProductsFragment
 import org.junit.After
@@ -16,11 +24,15 @@ import org.junit.Before
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Rule
 import org.junit.runner.RunWith
 
 @LargeTest
 @RunWith(AndroidJUnit4::class)
 class AboutAppFragmentTest {
+
+    @get:Rule
+    val intentsTestRule = IntentsTestRule(MainActivity::class.java)
 
     @Before
     fun setUp() {
@@ -41,6 +53,13 @@ class AboutAppFragmentTest {
 
     @Test
     fun privacy() {
+        onView(withId(R.id.privacy)).perform(click())
+        val chooser = Intents.getIntents().last()
+        assertEquals(chooser.action, Intent.ACTION_CHOOSER)
+        assertTrue(chooser.hasExtra(Intent.EXTRA_INTENT))
+        val intent = chooser.getParcelableExtra<Intent>(Intent.EXTRA_INTENT)
+        assertEquals(intent?.action, Intent.ACTION_VIEW)
+        assertEquals(intent?.data, Uri.parse(DataMock.aboutApp.privacy))
     }
 
     @Test
